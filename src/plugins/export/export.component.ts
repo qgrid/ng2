267b3bbb60adf1c4ab7b952fc8639a695aa8a521
Ekaterina Tokareva <Tokaree@labcorp.com>
2017-06-07 19:35:39 +0300

    export component

---
+++ src/plugins/export/export.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {PluginComponent} from '../plugin.component';
+
+@Component({
+    selector: 'q-grid-export',
+    template: require('./export.component.html')
+})
+
+export class ExportComponent extends PluginComponent{
+
+}
--- src/plugins/plugin.module.ts
+++ src/plugins/plugin.module.ts
@@ -1,13 +1,15 @@
 import {NgModule} from '@angular/core';
 import {PagerModule} from './pagination';
 import {ProgressModule} from './progress';
+import {ExportModule} from './export';
 
 @NgModule({
   declarations: [
   ],
   exports: [
     PagerModule,
-    ProgressModule
+    ProgressModule,
+    ExportModule
   ],
   imports: [
   ],
