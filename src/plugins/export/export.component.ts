7fb4ee1f37ab3b1862bf70ea110b09fd0b9f5ee0
Ekaterina Tokareva <Tokaree@labcorp.com>
2017-07-11 14:22:59 +0300

    export-plugin

--- src/plugins/export/export.component.ts
+++ src/plugins/export/export.component.ts
@@ -1,6 +1,9 @@
 import {Component, Input} from '@angular/core';
+// import {PluginService} from '@grid/core/plugin';
 import {PluginComponent} from '../plugin.component';
 import {Command} from '@grid/core/infrastructure';
+import {Csv} from '@grid/core/export/csv';
+// import {downloadFactory} from './download';
 
 @Component({
     selector: 'q-grid-export',
@@ -8,5 +11,30 @@ import {Command} from '@grid/core/infrastructure';
 })
 
 export class ExportComponent extends PluginComponent {
-    @Input() exportType;
+    @Input() type: string;
+
+    get rows() {
+        return this.root.model.data().rows;
+    }
+
+    get columns() {
+        return this.root.model.data().columns;
+    }
+
+    get id() {
+        return this.root.model.grid().id;
+    }
+
+    csv = new Command({
+        canExecute: () => this.type === 'csv',
+        execute: () => {
+            // const pluginService = new PluginService(this.model);
+            // const fileSaver = pluginService.resolve('fileSaver');
+            const csv = new Csv();
+            const data = csv.write(this.rows, this.columns);
+            // const download = downloadFactory(fileSaver);
+            // download(this.id, data, `text/${this.type}`);
+
+        }
+    });
 }
