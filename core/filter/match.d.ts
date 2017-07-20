cfa04d5e811831e653b5b0f3ca837a1f57080cfc
munkk <munkk@yandex.ru>
2017-07-20 14:59:52 +0300

    Update match.d.ts

--- core/filter/match.d.ts
+++ core/filter/match.d.ts
@@ -1,7 +1,5 @@
-import {Grid} from '..src/view/services';
-
-export declare interface IMatchResult{
-	(context: Grid): boolean;
+export declare interface IMatchResult {
+	(context: object): boolean;
 }
 
-export declare function match(context: Grid): boolean;
+export declare function match(context: object): boolean;
