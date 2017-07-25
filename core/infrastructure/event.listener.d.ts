202debbf8f17b5b2c6754296700dabb46ba882d6
munkk <munkk@yandex.ru>
2017-07-25 18:42:55 +0300

    Update event.listener.d.ts

--- core/infrastructure/event.listener.d.ts
+++ core/infrastructure/event.listener.d.ts
@@ -1,11 +1,12 @@
 import {IFunc} from '../dom/view';
+import {EventManager} from '@grid/core/infrastructure/event.manager';
 
 export interface IOnResult {
 	(): void;
 }
 
 export declare class EventListener {
-	constructor(context: object, element: object);
+	constructor(context: object, element: EventManager);
 	on(name: string, f: IFunc): IOnResult;
 	off(): void;
 }
