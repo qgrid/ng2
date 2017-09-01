f6e7f09c818e39c4d06cda59cfb8e9632bb67851
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-09-01 13:44:11 +0300

    Added toolbar component

---
+++ src/main/toolbar/toolbar.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+import {TemplateHostService} from 'ng2-qgrid/template';
+
+@Component({
+	selector: 'q-grid-toolbar',
+	template: '<ng-content></ng-content>',
+	providers: [TemplateHostService]
+})
+export class ToolbarComponent implements OnInit {
+	constructor(private templateHost: TemplateHostService) {
+		templateHost.key = 'toolbar.tpl.html';
+	}
+
+	ngOnInit() {
+	}
+}
