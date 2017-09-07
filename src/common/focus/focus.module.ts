e23be6e44a1bdbec4dcf6d4961ae1cc6bb345029
Azamat Kurbanaev <azamat.kurbanaev@arcadia.spb.ru>
2017-09-07 06:54:05 +0300

    refactoring + redesign

--- src/common/focus/focus.module.ts
+++ src/common/focus/focus.module.ts
@@ -1,15 +1,18 @@
 import {NgModule} from '@angular/core';
 import {FocusDirective} from './focus.directive';
 import {AutoFocusDirective} from './autofocus.directive';
+import {BlurDirective} from './blur.directive';
 
 @NgModule({
 	declarations: [
 		FocusDirective,
 		AutoFocusDirective,
+		BlurDirective
 	],
 	exports: [
 		FocusDirective,
 		AutoFocusDirective,
+		BlurDirective
 	],
 	imports: [],
 	providers: []
