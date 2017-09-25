1b598a3e581842a08fc542ca484c7105d9acaee3
klumba12 <dimitry.sitnov@gmail.com>
2017-09-25 09:43:01 +0300

    bind actions to action-bar

--- src/plugins/action-bar/action-core.component.ts
+++ src/plugins/action-bar/action-core.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Optional } from '@angular/core';
+import { AppError } from 'ng2-qgrid/core/infrastructure/error';
 import { Action as ActionItem } from 'ng2-qgrid/core/action/action';
 import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
 import { PluginComponent } from '../plugin.component';
@@ -8,40 +9,54 @@ import { PluginComponent } from '../plugin.component';
 	templateUrl: './action-core.component.html'
 })
 export class ActionCoreComponent extends PluginComponent {
-	@Input() public model: ActionItem = null;
+	@Input() public action: ActionItem = null;
 
 	constructor(@Optional() root: RootService) {
 		super(root);
 	}
 
 	execute() {
-		const model = this.model;
-		return model && model.command && model.command.execute();
+		const model = this.action;
+		if (!model) {
+			throw new AppError('action-core.component', 'Model shoud be setup');
+		}
+
+		return model.command.execute();
 	}
 
 	canExecute() {
-		const model = this.model;
-		return model && model.command && model.command.canExecute();
+		const model = this.action;
+		if (!model) {
+			throw new AppError('action-core.component', 'Model shoud be setup');
+		}
+
+		return model.command.canExecute();
 	}
 
 	get shortcut() {
-		const model = this.model;
-		if (model && model.command) {
-			return model.command.shortcut;
+		const model = this.action;
+		if (!model) {
+			throw new AppError('action-core.component', 'Model shoud be setup');
 		}
+
+		return model.command.shortcut;
 	}
 
 	get title() {
-		const model = this.model;
-		if (model) {
-			return model.title;
+		const model = this.action;
+		if (!model) {
+			throw new AppError('action-core.component', 'Model shoud be setup');
 		}
+
+		return model.title;
 	}
 
 	get icon() {
-		const model = this.model;
-		if (model) {
-			return model.icon;
+		const model = this.action;
+		if (!model) {
+			throw new AppError('action-core.component', 'Model shoud be setup');
 		}
+
+		return model.icon;
 	}
 }
