c7687558e5ee218587a0e2ce055768a0fad51e7b
munkk <munkk@yandex.ru>
2017-10-05 13:13:07 +0300

    Update highlight.pipe.test.ts

--- src/pipes/highlight/highlight.pipe.test.ts
+++ src/pipes/highlight/highlight.pipe.test.ts
@@ -11,4 +11,19 @@ describe('Pipe: HighlightPipe', () => {
 		expect(pipe.transform('addhighlightclass', 'highlight'))
 			.toBe('add<span class="q-grid-highlight-part">highlight</span>class');
 	});
+
+	it('text & search = 0 case', () => {
+		expect(pipe.transform(0, 0))
+			.toBe('<span class="q-grid-highlight-part">0</span>');
+	});
+	
+	it('text & search = null case', () => {
+		expect(pipe.transform(null, null))
+			.toBe('null');
+	});
+	
+	it('text & search = undefined case', () => {
+		expect(pipe.transform(undefined, undefined))
+			.toBe('undefined');
+	});
 });
