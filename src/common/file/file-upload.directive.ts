9bb5f44fc2791db1d8950710fa63ac9b08c46c8f
Azamat Kurbanaev <azamat.kurbanaev@arcadia.spb.ru>
2017-10-24 15:41:40 +0300

    file column type editor (+file upload directive) implemented

--- src/common/file/file-upload.directive.ts
+++ src/common/file/file-upload.directive.ts
@@ -10,15 +10,6 @@ import {
 } from '@angular/core';
 
 import { AppError } from 'ng2-qgrid/core/infrastructure';
-
-//import { OnDestroy, OnInit } from '@angular/core';
-//import { EventListener, EventManager } from 'ng2-qgrid/core/infrastructure';
-
-// const FILE_UPLOAD_NAME = `qGridFileUpload`;
-// const FILE_UPLOAD_LABEL_NAME = `qGridFileUploadLabel`;
-// const CAN_UPLOAD_NAME = `qGridCanUpload`;
-
-
 import { NgComponent, RootService } from 'ng2-qgrid/infrastructure/component';
 import { noop } from 'ng2-qgrid/core/utility';
 
@@ -26,22 +17,19 @@ import { noop } from 'ng2-qgrid/core/utility';
 @Directive({
 	selector: '[q-grid-file-upload]'
 })
-export class FileUpload /*extends NgComponent*/ implements AfterViewInit {
+export class FileUpload implements AfterViewInit {
 	@Input('q-grid-can-upload') canUpload: Function = noop;
 	@Input('q-grid-file-upload') selector;
 	@Input('q-grid-file-upload-label') label: string;
-	//@Output('q-grid-can-upload') canUpload: EventEmitter<boolean> = new EventEmitter<boolean>();
 
 	private file: File;
 
 	private reader: FileReader;
-	//private listener: EventListener;
 
 	constructor(@Optional()
 		private root: RootService,
 		private renderer: Renderer2,
 		private elementRef: ElementRef) {
-		//super();
 		this.reader = new FileReader();
 	}
 
@@ -57,17 +45,11 @@ export class FileUpload /*extends NgComponent*/ implements AfterViewInit {
 			);
 		}
 
-		//const listener = new EventListener(element, new EventManager(this));
-
 		this.reader.onloadend = e => this.setDataUrl(e);
 
 		this.renderer.listen(element, 'change', this.upload);
 		this.renderer.listen(element, 'click', this.onClick);
 		this.renderer.listen(element, 'drop', this.upload);
-
-		// this.using(this.listener.on('change', this.upload));
-		// this.using(this.listener.on('click', this.onClick));
-		// this.using(this.listener.on('drop', this.upload));
 	}
 
 	onClick() {
@@ -96,13 +78,4 @@ export class FileUpload /*extends NgComponent*/ implements AfterViewInit {
 			this.root.applyFactory(() => this.file = e.target.result);
 		}
 	}
-
-	// ngOnInit() {
-	// 	super.ngOnInit();
-	// }
-
-	// ngOnDestroy() {
-	// 	super.ngOnDestroy();
-	// 	this.dispose();
-	// }
 }
