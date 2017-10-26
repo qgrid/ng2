8c17414867ee3b9b221207e63c5bfe8b2fded942
RussianDjoNIK <zaharcev777@ya.ru>
2017-10-26 17:41:00 +0300

    fixed comments

--- src/common/raise/raise.directive.ts
+++ src/common/raise/raise.directive.ts
@@ -1,23 +1,22 @@
 import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
-import { Log } from 'ng2-qgrid/core/infrastructure';
+import { AppError } from 'ng2-qgrid/core/infrastructure';
 
 @Directive({
 	selector: '[q-grid-raise]'
 })
 export class RaiseDirective implements AfterViewInit {
 	@Input('q-grid-raise') public type = 'click';
+	@Input('q-grid-raise-target') public target:string;
 
 	constructor(private element: ElementRef) {}
 
 	ngAfterViewInit() {
 		const event = new Event(this.type);
 		const rootElem = this.element.nativeElement;
-		const raiseTarget = rootElem.attributes['q-grid-raise-target'];
 
-		let raiseElem = raiseTarget ? rootElem.querySelector(raiseTarget.value) : rootElem;
+		let raiseElem = this.target ? rootElem.querySelector(this.target) : rootElem;
 		if (!raiseElem) {
-			Log.warn('raise.directive', 'raise target not found');
-			raiseElem = rootElem;
+			throw new AppError('raise.directive', 'raise target not found');
 		}
 
 		raiseElem.dispatchEvent(event);
