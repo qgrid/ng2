6a1b32d893ee824a5d27b0185ad5c0b7b35040d8
klumba12 <dimitry.sitnov@gmail.com>
2017-12-04 12:13:20 -0500

    update box component using box.ctrl

--- src/main/box/box.component.ts
+++ src/main/box/box.component.ts
@@ -4,39 +4,29 @@ import { ThemeService } from 'ng2-qgrid/template/theme.service';
 import { GRID_PREFIX } from 'ng2-qgrid/core/definition';
 import { Guard } from 'ng2-qgrid/core/infrastructure';
 import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
+import { BoxCtrl } from 'ng2-qgrid/core/box/box.ctrl';
+import { Model } from 'ng2-qgrid/core/infrastructure/model';
 
 @Component({
 	selector: 'q-grid-box',
 	template: '<ng-content></ng-content>'
 })
 export class BoxComponent extends NgComponent implements OnInit {
-	@Input('model') private boxModel: any = null;
-	private element: HTMLElement = null;
+	@Input('model') private boxModel: Model = null;
+	private ctrl: BoxCtrl;
 
-	constructor( @Optional() private root: RootService, element: ElementRef, private theme: ThemeService) {
+	constructor( @Optional() private root: RootService, private element: ElementRef, private theme: ThemeService) {
 		super();
-
-		this.element = element.nativeElement;
 	}
 
 	ngOnInit() {
+		this.ctrl = new BoxCtrl(this.model, this.element.nativeElement);
 		this.initTheme();
-
-		const model = this.model;
-		this.using(model.dragChanged.watch(e => {
-			if (e.hasChanges('isActive')) {
-				if (model.drag().isActive) {
-					this.element.classList.add(`${GRID_PREFIX}-drag`);
-				} else {
-					this.element.classList.remove(`${GRID_PREFIX}-drag`);
-				}
-			}
-		}));
 	}
 
 	initTheme() {
-		const element = this.element;
-		element.classList.add(GRID_PREFIX);
+		const element = this.element.nativeElement;
+
 		this.using(this.theme.changed.watch(e => {
 			if (e) {
 				element.classList.remove(`${GRID_PREFIX}-theme-${e.oldValue}`);
