c8f8136ef3bc1cce5411ee6018f9bc3df934e8a3
klumba12 <dimitry.sitnov@gmail.com>
2017-12-06 08:37:50 -0500

    grid-component

---
+++ core/grid/grid.ctrl.d.ts
@@ -0,0 +1,20 @@
+import { Model } from '../infrastructure';
+import { View } from '../view/view';
+import { Table } from '../dom/table';
+import { Bag } from '../dom/bag';
+
+export declare class GridBag {
+	head: Bag;
+	body: Bag;
+	foot: bag;
+}
+
+export declare class GridCtrl extends View {
+	constructor(model: Model, context: any)
+
+	keyDown(e: any, source = 'grid');
+	invalidateActive(): void;
+
+	table: Table;
+	bag: GridBag;
+}
