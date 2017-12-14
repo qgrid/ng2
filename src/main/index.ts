5e4bdd6769a0e2e1ed64ea4fe0bb97cc5fde6e96
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-12-14 20:47:55 +0300

    added separate markup directive for the grid

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,47 +1,6 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { GridComponent } from './grid/grid.component';
-import { GridService } from './grid/grid.service';
-import { ColumnComponent, ColumnListComponent } from './column';
-import { BoxComponent } from './box';
-import { CoreModule } from './core';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { RowComponent } from './core/row/row.component';
-import { ThemeModule } from 'ng2-qgrid/themes/material/theme.module';
-
 export * from './box/index';
 export * from './column/index';
 export * from './core/index';
 export * from './grid/index';
 export * from './layer/index';
-
-@NgModule({
-	declarations: [
-		GridComponent,
-		BoxComponent,
-		ColumnListComponent,
-		ColumnComponent,
-		ToolbarComponent,
-		RowComponent
-	],
-	exports: [
-		GridComponent,
-		ColumnListComponent,
-		ColumnComponent,
-		BoxComponent,
-		ToolbarComponent,
-		RowComponent
-	],
-	imports: [
-		BrowserModule,
-		CoreModule,
-		ThemeModule
-	],
-	providers: [
-		GridService
-	]
-})
-export class MainModule {
-	constructor() {
-	}
-}
+export * from './main.module';
---
+++ src/main/main.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { GridComponent } from './grid/grid.component';
+import { GridService } from './grid/grid.service';
+import { ColumnComponent, ColumnListComponent } from './column';
+import { BoxComponent } from './box';
+import { CoreModule } from './core';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { RowComponent } from './core/row/row.component';
+import { ThemeModule } from 'ng2-qgrid/themes/material/theme.module';
+import { MarkupDirective } from './markup/markup.directive';
+
+@NgModule({
+	declarations: [
+		GridComponent,
+		BoxComponent,
+		ColumnListComponent,
+		ColumnComponent,
+		ToolbarComponent,
+		RowComponent,
+		MarkupDirective
+	],
+	exports: [
+		GridComponent,
+		ColumnListComponent,
+		ColumnComponent,
+		BoxComponent,
+		ToolbarComponent,
+		RowComponent
+	],
+	imports: [
+		BrowserModule,
+		CoreModule,
+		ThemeModule
+	],
+	providers: [
+		GridService
+	]
+})
+export class MainModule {
+	constructor() {
+	}
+}
