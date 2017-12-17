d3f17da699133929213a7500d826c846cab99ee9
klumba12 <dimitry.sitnov@gmail.com>
2017-12-17 11:41:04 +0300

    fix position init

--- src/common/layout/position.directive.ts
+++ src/common/layout/position.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Input, AfterViewInit, OnDestroy } from '@angular/core';
+import { Directive, ElementRef, Input, AfterViewInit, OnDestroy, OnInit } from '@angular/core';
 import { max } from 'ng2-qgrid/core/utility';
 import { RootService } from '../../infrastructure/component';
 import { PositionView } from 'ng2-qgrid/plugin/position/position.view';
@@ -6,13 +6,16 @@ import { PositionView } from 'ng2-qgrid/plugin/position/position.view';
 @Directive({
 	selector: '[q-grid-position]'
 })
-export class PositionDirective implements OnDestroy {
+export class PositionDirective implements OnDestroy, OnInit {
 	@Input('q-grid-position') public target = '';
 	private position: PositionView;
 
-	constructor(element: ElementRef, private root: RootService) {
+	constructor(private element: ElementRef, private root: RootService) {
+	}
+
+	ngOnInit() {
 		this.position = new PositionView({
-			element: element.nativeElement,
+			element: this.element.nativeElement,
 			targetName: this.target
 		});
 
