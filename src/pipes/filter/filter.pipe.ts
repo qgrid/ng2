5bfac9d05bf8493a5054aadccb67ea31e6f3519a
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-12-18 20:04:31 +0300

    fix progress layout + column filter

--- src/pipes/filter/filter.pipe.ts
+++ src/pipes/filter/filter.pipe.ts
@@ -1,17 +1,23 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { isObject } from 'ng2-qgrid/core/utility';
 
 @Pipe({
 	name: 'qGridFilter'
 })
 export class FilterPipe implements PipeTransform {
 	transform(items: any, filter: any): any {
-		if (filter) {
-			// TODO: improve perfomance
-			const keys = Object.keys(filter);
-			return items.filter(item =>
-				keys.reduce((memo, key) =>
-					(memo && new RegExp(filter[key], 'gi').test(item[key])) || filter[key] === '',
-					true));
+		if (filter || filter === 0 || filter === false) {
+			if (isObject(filter)) {
+				// TODO: improve perfomance
+				const keys = Object.keys(filter);
+				return items.filter(item =>
+					keys.reduce((memo, key) =>
+						(memo && new RegExp(filter[key], 'gi').test(item[key])) || filter[key] === '',
+						true));
+			}
+
+			const expr = new RegExp(filter, 'gi');
+			return items.filter(item => expr.test(item));
 		}
 
 		return items;
