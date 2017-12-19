a02732aed2af34c65033a075c6d2dd3b8f16a42c
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-12-19 14:45:56 +0300

    fix select all btn

--- src/main/grid/grid.service.ts
+++ src/main/grid/grid.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Model } from 'ng2-qgrid/core/infrastructure/model';
 import { Action } from 'ng2-qgrid/core/action/action';
 import { Command } from 'ng2-qgrid/core/command/command';
@@ -10,11 +10,11 @@ import { getFactory as labelFactory } from 'ng2-qgrid/core/services/label';
 import { RowDetailsStatus } from 'ng2-qgrid/core/row-details/row.details.status';
 import { Log } from 'ng2-qgrid/core/infrastructure/log';
 import { identity, noop } from 'ng2-qgrid/core/utility';
+import { setTimeout } from 'timers';
 
 @Injectable()
 export class GridService {
-	constructor() {
-	}
+	constructor(private zone: NgZone) {}
 
 	model() {
 		return new Model();
@@ -24,27 +24,36 @@ export class GridService {
 		const start = () => {
 			Log.info('service', 'invalidate start');
 
-			model.scene({
-				status: 'start'
-			}, {
+			model.scene(
+				{
+					status: 'start'
+				},
+				{
 					source: 'grid',
 					behavior: 'core'
-				});
+				}
+			);
 
 			return job => {
 				const scene = model.scene;
-				scene({
-					round: scene().round + 1
-				}, {
+				scene(
+					{
+						round: scene().round + 1
+					},
+					{
 						source: 'grid',
 						behavior: 'core'
+					}
+				);
+
+				return new Promise(resolve => {
+					this.zone.run(() => {
+						if (job) {
+							job();
+						}
 					});
-
-				if (job) {
-					job();
-				}
-
-				return Promise.resolve();
+					resolve();
+				});
 			};
 		};
 
