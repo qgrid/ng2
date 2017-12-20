206ba571345eec2f2084c9d5105e7b3be2c2fd76
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-12-20 18:24:50 +0300

    fix dts

--- plugin/column-filter/column.filter.view.d.ts
+++ plugin/column-filter/column.filter.view.d.ts
@@ -1,10 +1,13 @@
 import { PluginView } from '../plugin.view';
-import { Model } from '../../core/infrastructure/model'
-import { Event } from '../../core/infrastructure/event'
+import { Model } from '../../core/infrastructure/model';
+import { Event } from '../../core/infrastructure/event';
+import { ColumnModel } from '../../core/column-list/column.list.model';
 
 export declare class ColumnFilterView extends PluginView {
-    constructor(model: Model, context: any);
-    cancelEvent: Event;
-    submitEvent: Event;
-    getValue: (row: any) => any;
+	constructor(model: Model, context: any);
+	cancelEvent: Event;
+	submitEvent: Event;
+	getValue: (row: any) => any;
+	column: ColumnModel;
+	items: Array<any>;
 }
