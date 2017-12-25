ff93611ce9c889f25588269c9b70adde66ae1f9b
Andrey <munkk@yandex.ru>
2017-12-25 17:54:47 +0300

    Right click and mouse wheel click on backdrop now close editor

--- plugin/backdrop/backdrop.view.js
+++ plugin/backdrop/backdrop.view.js
@@ -1,25 +1,33 @@
 import {PluginView} from '../plugin.view';
 import {Event, EventListener, EventManager} from '../../core/infrastructure';
 
+const mouse_left_button = 1;
+const mouse_wheel_button = 2;
+
 export class BackdropView extends PluginView {
 	constructor(context) {
 		super(context);
-		
+
 		this.closeEvent = new Event();
 
 		const element = context.element;
 		const listener = new EventListener(element, new EventManager(this));
 
-		this.using(listener.on('click', e => {
-			e.stopPropagation();
-			element.remove();
+		this.using(listener.on('mouseup', e => {
 
-			if (context.propagate !== false) {
-				const target = document.elementFromPoint(e.clientX, e.clientY);
-				target.click();
+			if (e.which === mouse_left_button || e.which === mouse_wheel_button) {
+
+				e.stopPropagation();
+				element.remove();
+
+				if (context.propagate !== false) {
+					const target = document.elementFromPoint(e.clientX, e.clientY);
+					target.click();
+				}
+
+				this.closeEvent.emit(e);
+
 			}
-
-			this.closeEvent.emit(e);
 		}));
 
 		this.using(listener.on('keydown', e => {
