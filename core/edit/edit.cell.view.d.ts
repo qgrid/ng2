06e00b46f89d33205466ba2ad504dd8f60476fd1
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2017-12-25 18:45:00 +0300

    fix formatting

--- core/edit/edit.cell.view.d.ts
+++ core/edit/edit.cell.view.d.ts
@@ -1,11 +1,11 @@
-import {CellEditor} from './edit.cell.editor';
-import {CommandManager} from '../command/command.manager';
-import {Command} from '../command/command';
-import {INoopResult} from '../utility/utility';
-import {ColumnModel, IEditorOptions} from '../column-type/column.model';
-import {Table} from '../dom/table';
-import {Model} from '../infrastructure/model';
-import {View} from '../view/view';
+import { CellEditor } from './edit.cell.editor';
+import { CommandManager } from '../command/command.manager';
+import { Command } from '../command/command';
+import { INoopResult } from '../utility/utility';
+import { ColumnModel, IEditorOptions } from '../column-type/column.model';
+import { Table } from '../dom/table';
+import { Model } from '../infrastructure/model';
+import { View } from '../view/view';
 
 export declare class EditCellView extends View {
 	constructor(model: Model, table: Table, commandManager: CommandManager);
@@ -14,11 +14,11 @@ export declare class EditCellView extends View {
 	enter: Command;
 	commit: Command;
 	cancel: Command;
-    reset: Command;
-    exit: Command;
+	reset: Command;
+	exit: Command;
 	value: any;
-    label: any;
-    column: ColumnModel;
+	label: any;
+	column: ColumnModel;
 	readonly fetch: INoopResult;
 	readonly options: IEditorOptions;
 }
