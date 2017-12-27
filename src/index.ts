a85c28c9291f3a9783b14207399e74a3ec01bb1f
valentin.korolev <valentin.korolev@arcadia.spb.ru>
2017-12-27 13:25:24 +0300

    Export core grid service

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export {GridModule} from './grid.module';
-export {GridService} from './main/grid/grid.service';
+export {GridService as Grid} from './main/grid/grid.service';
+export {GridService} from 'ng2-qgrid/core/services/grid';
 export {Model as GridModel} from 'ng2-qgrid/core/infrastructure/model';
 export {ThemeModule} from 'ng2-qgrid/themes/material';
 
