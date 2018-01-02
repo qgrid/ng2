7fcc1633443521430112fec381f5d6f3628225b5
klumba12 <dimitry.sitnov@gmail.com>
2018-01-02 11:47:06 +0300

    disable zone for dnd

--- src/common/dnd/drop.directive.ts
+++ src/common/dnd/drop.directive.ts
@@ -1,82 +1,85 @@
-import { Directive, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Optional, Output } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Optional, Output, NgZone } from '@angular/core';
 import { EventManager } from 'ng2-qgrid/core/infrastructure';
 import { EventListener } from 'ng2-qgrid/core/infrastructure/event.listener';
 import { DragService } from './drag.service';
 import { GRID_PREFIX } from 'ng2-qgrid/core/definition';
 
 @Directive({
-	selector: '[q-grid-drop]'
+    selector: '[q-grid-drop]'
 })
 export class DropDirective implements OnInit, OnDestroy {
-	private element: Element;
-	private listener;
+    private element: Element;
+    private listener;
 
-	@Input('q-grid-drop') transfer;
-	@Input('q-grid-drop-effect') effect;
-	@Input('q-grid-can-drop') canDrop;
-	@Output('q-grid-on-drop') onDrop = new EventEmitter<any>();
+    @Input('q-grid-drop') transfer;
+    @Input('q-grid-drop-effect') effect;
+    @Input('q-grid-can-drop') canDrop;
+    @Output('q-grid-on-drop') onDrop = new EventEmitter<any>();
 
-	constructor(elementRef: ElementRef) {
-		this.element = elementRef.nativeElement;
-		this.listener = new EventListener(this.element, new EventManager(this));
-	}
+    constructor(elementRef: ElementRef, private zone: NgZone) {
+        this.element = elementRef.nativeElement;
+        this.listener = new EventListener(this.element, new EventManager(this));
+    }
 
-	ngOnInit() {
-		this.element.classList.add(`${GRID_PREFIX}-can-drop`);
-		this.listener.on('dragenter', this.enter);
-		this.listener.on('dragover', this.over);
-		this.listener.on('dragleave', this.leave);
-		this.listener.on('drop', this.drop);
-	}
+    ngOnInit() {
+        this.element.classList.add(`${GRID_PREFIX}-can-drop`);
+        this.zone.runOutsideAngular(() => {
+            this.listener.on('dragenter', this.enter);
+            this.listener.on('dragover', this.over);
+            this.listener.on('dragleave', this.leave);
+        });
 
-	ngOnDestroy() {
-		this.element.classList.remove(`${GRID_PREFIX}-can-drop`);
-		this.listener.off();
-	}
+        this.listener.on('drop', this.drop);
+    }
 
-	drop(e) {
-		e.stopPropagation();
+    ngOnDestroy() {
+        this.element.classList.remove(`${GRID_PREFIX}-can-drop`);
+        this.listener.off();
+    }
 
-		this.element.classList.remove(`${GRID_PREFIX}-dragover`);
-		const event = this.event(e.dataTransfer);
-		if (this.canDrop(event)) {
-			this.onDrop.emit(event);
-		}
+    drop(e) {
+        e.stopPropagation();
 
-		return false;
-	}
+        this.element.classList.remove(`${GRID_PREFIX}-dragover`);
+        const event = this.event(e.dataTransfer);
+        if (this.canDrop(event)) {
+            this.onDrop.emit(event);
+        }
 
-	enter(e) {
-		e.preventDefault();
+        return false;
+    }
 
-		this.element.classList.add(`${GRID_PREFIX}-dragover`);
-		e.dataTransfer.dropEffect = this.effect || 'move';
-		return false;
-	}
+    enter(e) {
+        e.preventDefault();
 
-	over(e) {
-		e.preventDefault();
+        this.element.classList.add(`${GRID_PREFIX}-dragover`);
+        e.dataTransfer.dropEffect = this.effect || 'move';
+        return false;
+    }
 
-		let effect = this.effect || 'move';
-		if (this.element.classList.contains(`${GRID_PREFIX}-drag`) ||
-			this.canDrop(this.event()) === false) {
-			effect = 'none';
-		}
+    over(e) {
+        e.preventDefault();
 
-		e.dataTransfer.dropEffect = effect;
-		return false;
-	}
+        let effect = this.effect || 'move';
+        if (this.element.classList.contains(`${GRID_PREFIX}-drag`) ||
+            this.canDrop(this.event()) === false) {
+            effect = 'none';
+        }
 
-	leave() {
-		this.element.classList.remove(`${GRID_PREFIX}-dragover`);
-	}
+        e.dataTransfer.dropEffect = effect;
+        return false;
+    }
 
-	event(e?) {
-		const target = this.transfer;
-		const source = arguments.length
-			? DragService.decode(e.getData(DragService.mimeType))
-			: DragService.transfer;
+    leave() {
+        this.element.classList.remove(`${GRID_PREFIX}-dragover`);
+    }
 
-		return { source, target };
-	}
+    event(e?) {
+        const target = this.transfer;
+        const source = arguments.length
+            ? DragService.decode(e.getData(DragService.mimeType))
+            : DragService.transfer;
+
+        return { source, target };
+    }
 }
