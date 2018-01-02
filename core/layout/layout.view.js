d117530a34cc4f13329502bb23ceff43af4e4587
klumba12 <dimitry.sitnov@gmail.com>
2018-01-02 12:42:52 +0300

    fix tabs

--- core/layout/layout.view.js
+++ core/layout/layout.view.js
@@ -5,121 +5,121 @@ import { clone } from '../utility';
 import { Log, Composite } from '../infrastructure';
 
 export class LayoutView extends View {
-    constructor(model, table, service) {
-        super(model);
-        this.model = model;
-        this.table = table;
-        this.service = service;
+	constructor(model, table, service) {
+		super(model);
+		this.model = model;
+		this.table = table;
+		this.service = service;
 
-        this.onInit();
-    }
+		this.onInit();
+	}
 
-    onInit() {
-        const model = this.model;
+	onInit() {
+		const model = this.model;
 
-        model.sceneChanged.watch(e => {
-            if (e.hasChanges('column')) {
-                this.invalidateColumns();
-            }
-        });
+		model.sceneChanged.watch(e => {
+			if (e.hasChanges('column')) {
+				this.invalidateColumns();
+			}
+		});
 
-        const styleRow = this.styleRow.bind(this);
-        model.layoutChanged.watch(e => {
-            if (e.hasChanges('columns')) {
-                const form = this.getColumnForm();
-                this.invalidateColumns(form);
-            }
-        });
+		const styleRow = this.styleRow.bind(this);
+		model.layoutChanged.watch(e => {
+			if (e.hasChanges('columns')) {
+				const form = this.getColumnForm();
+				this.invalidateColumns(form);
+			}
+		});
 
-        model.rowChanged.watch(e => {
-            if (e.hasChanges('canResize')) {
-                const rows = Array.from(model.style().rows);
-                if (e.state.canResize) {
-                    rows.push(styleRow);
-                }
-                else {
-                    const index = model.style.rows.indexOf(styleRow);
-                    rows.splice(index, 1);
-                }
-                model.style({ rows });
-            }
-        });
-    }
+		model.rowChanged.watch(e => {
+			if (e.hasChanges('canResize')) {
+				const rows = Array.from(model.style().rows);
+				if (e.state.canResize) {
+					rows.push(styleRow);
+				}
+				else {
+					const index = model.style.rows.indexOf(styleRow);
+					rows.splice(index, 1);
+				}
+				model.style({ rows });
+			}
+		});
+	}
 
-    getRowForm() {
-        const model = this.model;
-        const layout = model.layout;
-        return clone(layout().rows);
-    }
+	getRowForm() {
+		const model = this.model;
+		const layout = model.layout;
+		return clone(layout().rows);
+	}
 
-    getColumnForm() {
-        const model = this.model;
-        const layout = model.layout;
-        const state = clone(layout().columns);
-        const headRow = this.table.head.row(0);
-        if (headRow) {
-            const columns = this.table.data.columns();
-            let length = columns.length;
-            while (length--) {
-                const column = columns[length];
-                if (!state.has(column.key)) {
-                    if (column.canResize) {
-                        const index = columns.findIndex(c => c === column);
-                        state.set(column.key, { width: headRow.cell(index).width() });
-                    }
-                }
-            }
-        }
+	getColumnForm() {
+		const model = this.model;
+		const layout = model.layout;
+		const state = clone(layout().columns);
+		const headRow = this.table.head.row(0);
+		if (headRow) {
+			const columns = this.table.data.columns();
+			let length = columns.length;
+			while (length--) {
+				const column = columns[length];
+				if (!state.has(column.key)) {
+					if (column.canResize) {
+						const index = columns.findIndex(c => c === column);
+						state.set(column.key, { width: headRow.cell(index).width() });
+					}
+				}
+			}
+		}
 
-        return state;
-    }
+		return state;
+	}
 
-    invalidateColumns(form) {
-        Log.info('layout', 'invalidate columns');
+	invalidateColumns(form) {
+		Log.info('layout', 'invalidate columns');
 
-        const table = this.table;
-        const getWidth = columnService.widthFactory(table, form);
-        const columns = table.data.columns();
-        const style = {};
+		const table = this.table;
+		const getWidth = columnService.widthFactory(table, form);
+		const columns = table.data.columns();
+		const style = {};
 
-        let length = columns.length;
-        while (length--) {
-            const column = columns[length];
-            const width = getWidth(column.key);
-            if (null !== width) {
-                const key = css.escape(column.key);
-                const size = width + 'px';
-                const sizeStyle = {
-                    'width': size,
-                    'min-width': size,
-                    'max-width': size
-                };
+		let length = columns.length;
+		while (length--) {
+			const column = columns[length];
+			const width = getWidth(column.key);
+			if (null !== width) {
+				const key = css.escape(column.key);
+				const size = width + 'px';
+				const sizeStyle = {
+					'width': size,
+					'min-width': size,
+					'max-width': size
+				};
 
-                style[`td.q-grid-${key}`] = sizeStyle;
-                style[`th.q-grid-${key}`] = sizeStyle;
-            }
-        }
+				style[`td.q-grid-${key}`] = sizeStyle;
+				style[`th.q-grid-${key}`] = sizeStyle;
+			}
+		}
 
-        const sheet = css.sheet(this.gridId, 'layout-column');
-        sheet.set(style);
-    }
+		const sheet = css.sheet(this.gridId, 'layout-column');
+		sheet.set(style);
+	}
 
-    styleRow(row, context) {
-        const form = this.getRowForm();
-        const style = form.get(row);
-        if (style) {
-            context.class(`resized-${style.height}px`, { height: style.height + 'px' });
-        }
-    }
+	styleRow(row, context) {
+		const form = this.getRowForm();
+		const style = form.get(row);
+		if (style) {
+			context.class(`resized-${style.height}px`, { height: style.height + 'px' });
+		}
+	}
 
-    dispose() {
-        super.dispose();
+	dispose() {
+		super.dispose();
 
-        const columnSheet = css.sheet(this.gridId, 'layout-column');
-        columnSheet.remove();
-    }
+		const columnSheet = css.sheet(this.gridId, 'layout-column');
+		columnSheet.remove();
+	}
 
-    get gridId() {
-        return this.model.grid().id;
-    }
+	get gridId() {
+		return this.model.grid().id;
+	}
 }
