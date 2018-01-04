b279b06116191909dc836db53c6d479204518cee
klumba12 <dimitry.sitnov@gmail.com>
2018-01-05 00:50:03 +0300

    start comments for highlight..model

--- core/highlight/highlight.model.d.ts
+++ core/highlight/highlight.model.d.ts
@@ -1,7 +1,15 @@
 import {ColumnModel} from '../column-type/column.model';
 
+/**
+ * A class contains highlight data for rows and columns
+ */
 export declare  class HighlightModel {
-	constructor();
+	/**
+	 * Set of columns that should be highlighted.
+	 */
 	columns: ColumnModel[];
+	/**
+	 * Set of rows that should be highlighted.
+	 */
 	rows: any[];
 }
