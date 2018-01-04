41741b987d9c0e26e90bb8850cd8017040397fc4
klumba12 <dimitry.sitnov@gmail.com>
2018-01-05 00:26:02 +0300

    start comments for filter.model

--- core/filter/filter.model.d.ts
+++ core/filter/filter.model.d.ts
@@ -1,11 +1,21 @@
-import {Resource} from '../resource/resource';
-import {IMatchResult} from './match';
-import {INoopResult} from '../utility/utility';
+import { Resource } from '../resource/resource';
+import { IMatchResult } from './match';
+import { INoopResult } from '../utility/utility';
 
+/**
+ * A class to setup data filters and expressions.
+ */
 export declare class FilterModel {
-	constructor();
 	resource: Resource;
+
 	by: object;
+	/**
+	 * Filter representation enum:
+	 * 1. `default` - filtration through column filters and external plugins.
+	 * 2. `row` - filtration through header row filter and external plugins.
+	 */
+	unit: string;
 	match: IMatchResult;
 	fetch: INoopResult;
+	assertFactory: any;
 }
