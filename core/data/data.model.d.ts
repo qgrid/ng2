d4abe0758c6a506057abfec7cca5395432cfabd7
klumba12 <dmitry.sitnov@arcadia.spb.ru>
2018-01-11 13:20:54 +0300

    add usage

--- core/data/data.model.d.ts
+++ core/data/data.model.d.ts
@@ -3,13 +3,14 @@ import { IPipe } from '../pipe/pipe.item';
 
 /**
  * A class that gives access to the high level grid data structures.
+ *
+ * ## Usage
  * Usually grid user can define this properties in different places.
- * 
  * Inside html through attribute bindings:
  * ```html
  * <q-grid [columns]="userColumns" [rows]="userRows">
  * ```
- * 
+ *
  * Inside html throught component:
  * ```html
  * <q-grid>
@@ -19,12 +20,12 @@ import { IPipe } from '../pipe/pipe.item';
  * 	</q-grid-columns>
  * </q-grid>
  * ```
- * 
+ *
  * Inside js code throught model:
  * ```javascript
  * const userRows = [];
  * const userColumns = [];
- * 
+ *
  * gridModel.data({
  *  rows: userRows,
  *  columns: userColumns
@@ -32,10 +33,10 @@ import { IPipe } from '../pipe/pipe.item';
  * ```
  */
 export declare class DataModel {
-    
+
 	/**
 	 * List of rows to display.
-	 * Rows property can be filled from the grid rows html or grid model data rows 
+	 * Rows property can be filled from the grid rows html or grid model data rows
      * property.
 	 */
     rows: any[];
@@ -43,14 +44,14 @@ export declare class DataModel {
 	/**
 	 * Set of columns to display.
 	 * Usually data columns can be setup from 3 places:
-     * 
+     *
      * 1. Columns binding property.
      * 2. Columns component.
      * 3. Grid model data columns property.
-     * 
+     *
 	 * We can have 3 sources of columns because each column has `key` property,
-     * that allows to make a merge. If you have defined columns in javascript and in template 
-     * with the same key, algorithm will try persist settings from both sources but 
+     * that allows to make a merge. If you have defined columns in javascript and in template
+     * with the same key, algorithm will try persist settings from both sources but
      * javascript will have top priority.
 	 */
     columns: ColumnModel[];
