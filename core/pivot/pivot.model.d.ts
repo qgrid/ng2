6907e4544e066460a8a782885a384c6ab94689f1
klumba12 <dimitry.sitnov@gmail.com>
2018-01-13 20:00:35 +0300

    upd-log2

--- core/pivot/pivot.model.d.ts
+++ core/pivot/pivot.model.d.ts
@@ -1,7 +1,14 @@
 import {Resource} from '../resource/resource';
 
+/**
+ * A class to control q-grid pivoting functions.
+ */
 export declare class PivotModel {
 	constructor();
 	resource: Resource;
+
+	/**
+	 * A list of column keys to pivot. Each item represents next level.
+	 */
 	by: any[];
 }
