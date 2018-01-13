6f86674d5be9dcd1bdcd16494f732cd7daa9dfb6
klumba12 <dimitry.sitnov@gmail.com>
2018-01-13 18:28:08 +0300

    command.manager doc

--- core/command/command.manager.d.ts
+++ core/command/command.manager.d.ts
@@ -1,9 +1,24 @@
-import {Command} from './command';
 
+/**
+ * Command resolution manager that controls what commands and in which order columns should be executed.
+ * Usually other q-grid internal services use this class after some keyboard events.
+ */
 export declare class CommandManager {
 	constructor();
 
+	/**
+	 * Execute commands in a manager specific way.
+	 * 
+	 * @param commands list of commands to execute, usually come after filter invokation.
+	 * @param source indicates a source of command execution.
+	 */
 	invoke(commands: Command[], source?: string): boolean;
 
+	/**
+	 * Filter out disabled commands, usually command `canExecute` method is used.
+	 * 
+	 * @param commands list of commands that are candidates for execution.
+	 * @param source indicates a source of command execution.
+	 */
 	filter(commands: Command[], source?: string): Command[];
 }
