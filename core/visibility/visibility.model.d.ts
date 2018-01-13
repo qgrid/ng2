03826d0e59f41a8473b4ca67e85e1cebf8c85713
klumba12 <dimitry.sitnov@gmail.com>
2018-01-13 20:34:30 +0300

    visibility.model doc

--- core/visibility/visibility.model.d.ts
+++ core/visibility/visibility.model.d.ts
@@ -12,14 +12,39 @@ export interface IPin {
 	right: boolean;
 }
 
+/**
+ * A class to control visibility q-grid areas visibility .
+ */
 export declare class VisibilityModel {
 	constructor();
 
 	resource: Resource;
+
+	/**
+	 * Indicates if the q-grid head is visible. 
+	 */
 	head: boolean;
+
+	/**
+	 * Indicates if the q-grid foot is visible. 
+	 */
 	foot: boolean;
+
+	/**
+	 * Indicates if the q-grid body is visible. 
+	 */
 	body: boolean;
+
+	/**
+	 * Controls the q-grid toolbar panels are visible. 
+	 * 
+	 * * `'top'` show/hide top toolbar.
+	 * * `'right'` show/hide right toolbar.
+	 * * `'bottom'` show/hide bottom toolbar.
+	 * * `'left'` show/hide left toolbar.
+	 */
 	toolbar: IToolbar;
+	
 	pin: IPin;
 	plugin: object;
 }
