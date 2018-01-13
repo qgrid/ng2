cd1000a44b5d09bd58337a839e7c168087a631ce
klumba12 <dimitry.sitnov@gmail.com>
2018-01-13 20:12:40 +0300

    scroll.model doc

--- core/scroll/scroll.model.d.ts
+++ core/scroll/scroll.model.d.ts
@@ -1,7 +1,29 @@
+/**
+ * A class that contains properties about scroll status.
+ */
 export declare class ScrollModel {
 	constructor();
+
+	/**
+	 * Scroll mode.
+	 * 
+	 * * `'default'` mode without virtualization.
+	 * * `'virtual'` turn on virtual mode.
+	 */
 	mode: string;
+
+	/**
+	 * Top scroll position.
+	 */
 	top: number;
+
+	/**
+	 * Left scroll position.
+	 */
 	left: number;
+
+	/**
+	 * Row index on top of the q-grid client area.
+	 */
 	cursor: number;
 }
