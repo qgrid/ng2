f618fd74c93f148d2be6d00f5fb0fc503bd91904
Max <maxkvt@gmail.com>
2018-01-24 12:40:22 +0300

    Fix code style issues

--- core/sort/sort.service.js
+++ core/sort/sort.service.js
@@ -1,9 +1,9 @@
 import { isString } from '../utility';
 
-export const key = newKey;
-export const index = newIndex;
-export const direction = newValue;
-export const map = newMap;
+export const key = getKey;
+export const index = getIndex;
+export const direction = getValue;
+export const map = getMap;
 
 export function orderFactory(model) {
 	const sort = model.sort;
@@ -17,18 +17,17 @@ export function orderFactory(model) {
 					return memo;
 				}, {});
 
-			by.sort((x, y) => indexMap[key(x)] - indexMap[key(y)]);
+			by.sort((x, y) => indexMap[getKey(x)] - indexMap[getKey(y)]);
 		}
 
 		return by;
 	};
 }
 
-function newKey(pair) {
-	let key = '';
-	if (!isString(pair)) {
-		key = Object.keys(pair)[0];
-	} else {
+function getKey(pair) {
+	let key = Object.keys(pair)[0];
+    
+    if (isString(pair)) {
 		key = pair.split(/[+-]/)[1];
 	}
 	
@@ -41,16 +40,15 @@ function newKey(pair) {
 	return key;
 }
 
-function newValue(pair) {
-	let value = '';
+function getValue(pair) {
 	if (!isString(pair)) {
-		const pairKey = key(pair);
-		value = pair[pairKey];
+		const pairKey = getKey(pair);
+		return pair[pairKey];
 	} else {
-		let delimiterSet = [{'desc':'-'}, {'asc':'+'}];
-		let direction = pair.split(pair.split(/[+-]/)[1])[0];
+		const delimiterSet = [{'desc':'-'}, {'asc':'+'}];
+		const direction = pair.split(pair.split(/[+-]/)[1])[0];
 
-		value = delimiterSet.map(obj => {
+		return delimiterSet.map(obj => {
 			return Object.keys(obj).map(key => {
 				let value = obj[key];
 				if (value === direction){
@@ -62,18 +60,16 @@ function newValue(pair) {
 		}).filter(v => v)
 		.reduce((p, k) => k, '');
 	}
-	
-	return value;
 }
 
-function newMap(pairs) {
+function getMap(pairs) {
 	return pairs.reduce((memo, pair) => {
-		const pairKey = key(pair);
-		memo[pairKey] = newValue(pair);
+		const pairKey = getKey(pair);
+		memo[pairKey] = getValue(pair);
 		return memo;
 	}, {});
 }
 
-function newIndex(pairs, pairKey) {
-	return pairs.map(newKey).findIndex(k => k === pairKey);
+function getIndex(pairs, pairKey) {
+	return pairs.map(getKey).findIndex(k => k === pairKey);
 }
