6adea3991cea1a89797cffb4b9bdae4e89779ee7
Maxim Titov <maxim.titov@arcadia.spb.ru>
2018-02-06 17:38:07 +0300

    sync changes

--- plugin/persistence/persistence.view.js
+++ plugin/persistence/persistence.view.js
@@ -200,7 +200,7 @@ export class PersistenceView extends PluginView {
 			const target = {};
 			model[key] = target;
 			for (const p of settings[key]) {
-				target[p] = key === 'filter' ? {} : [];
+			    target[p] = key === 'filter' ? {} : [];
 			}
 		}
 
