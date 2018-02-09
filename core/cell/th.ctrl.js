75be4c9a4d773de1a09229cf9a5b91b9aefc5849
klumba12 <dimitry.sitnov@gmail.com>
2018-02-09 19:47:14 +0300

    fix formattin

--- core/cell/th.ctrl.js
+++ core/cell/th.ctrl.js
@@ -2,29 +2,29 @@ import { GRID_PREFIX } from '../definition';
 import * as css from '../services/css';
 
 export class ThCtrl {
-    static classify(element, column) {
-        if (column.canEdit) {
-            element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-edit`));
-        }
+	static classify(element, column) {
+		if (column.canEdit) {
+			element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-edit`));
+		}
 
-        if (column.canResize) {
-            element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-resize`));
-        }
+		if (column.canResize) {
+			element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-resize`));
+		}
 
-        if (column.canSort) {
-            element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-sort`));
-        }
+		if (column.canSort) {
+			element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-sort`));
+		}
 
-        if (column.canMove) {
-            element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-move`));
-        }
+		if (column.canMove) {
+			element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-move`));
+		}
 
-        if (column.canFilter) {
-            element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-filter`));
-        }
+		if (column.canFilter) {
+			element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-filter`));
+		}
 
-        if (column.canHighlight) {
-            element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-highlight`));
-        }
-    }
+		if (column.canHighlight) {
+			element.classList.add(css.escapeAttr(`${GRID_PREFIX}-can-highlight`));
+		}
+	}
 }
