71c6e8efc60888bb0494d248dfa29a6c26fb809d
Maxim Titov <maxim.titov@arcadia.spb.ru>
2018-02-16 19:39:20 +0300

    fix typings

--- core/services/grid.d.ts
+++ core/services/grid.d.ts
@@ -1,12 +1,15 @@
 import { INoopResult } from '../utility/utility';
 import { Model } from '../infrastructure/model';
 import { IPipe } from '../pipe/pipe.item';
+import { PersistenceService } from '../persistence/persistence.service';
 
 /**
  * > Under Construction.
  */
 export declare class GridService {
-    constructor(model: Model);
+	 constructor(model: Model);
+
+	 state: PersistenceService;
 
     invalidate(source?: string, changes?: object, pipe?: IPipe<any>[]): Promise<any>;
 
