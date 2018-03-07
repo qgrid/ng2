99ba4b5f2c4560f08dd6584103c65f9510b48342
Andrey <munkk@yandex.ru>
2018-03-07 16:10:15 +0300

    Add styles

--- src/plugins/pagination/pager-target/pager-target.component.ts
+++ src/plugins/pagination/pager-target/pager-target.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Optional, OnDestroy, OnInit } from '@angular/core';
+import { Component, Optional, OnInit } from '@angular/core';
 import { PluginComponent } from '../../plugin.component';
 import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
 
@@ -7,22 +7,34 @@ import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
 	templateUrl: './pager-target.component.html'
 })
 export class PagerTargetComponent extends PluginComponent implements OnInit {
-	constructor( @Optional() root: RootService) {
+	constructor(@Optional() root: RootService) {
 		super(root);
 	}
 
+	private value: string;
 	private target: any;
 
 	ngOnInit() {
 		this.target = this;
-		this.context = { $implicit: this.target };
+		this.context = {$implicit: this.target};
 	}
 
 	setPage(page: string) {
-		const pageNumber = Number.parseInt(page) - 1;
-		const pagination = this.model.pagination();
+		const pageNumber = Number.parseInt(page);
+		const maxAllowed = this.total();
 
-		pagination.current = pageNumber;
+		if (pageNumber > 0 && pageNumber <= maxAllowed) {
+			this.model.pagination({current: pageNumber - 1});
+		} else {
+			this.value = '1';
+		}
 	}
 
+	total() {
+		const pagination = this.model.pagination();
+		const count = pagination.count;
+		const size = pagination.size;
+
+		return count / size;
+	}
 }
