eef44f6a36b4bd1cbf5dc47e73690dbf9cb9daf2
Andrey <munkk@yandex.ru>
2018-03-13 16:55:17 +0300

    Fixed event listener

--- src/plugins/pagination/pager-target/pager-target.component.ts
+++ src/plugins/pagination/pager-target/pager-target.component.ts
@@ -1,23 +1,20 @@
-import {
-	Component, Optional, OnInit, HostListener, ViewChild, ElementRef, AfterViewInit,
-	ChangeDetectorRef
-} from '@angular/core';
+import { Component, Optional, OnInit } from '@angular/core';
 import { PluginComponent } from '../../plugin.component';
 import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
-import {Observable} from 'rxjs/Observable';
-
 
 @Component({
 	selector: 'q-grid-pager-target',
 	templateUrl: './pager-target.component.html'
 })
+
 export class PagerTargetComponent extends PluginComponent implements OnInit {
-	constructor(@Optional() root: RootService, private cdRef: ChangeDetectorRef) {
+	constructor(@Optional() root: RootService) {
 		super(root);
 	}
 
 	private value: any;
 	private target: any;
+	private input: HTMLInputElement;
 
 	ngOnInit() {
 		this.target = this;
@@ -25,31 +22,23 @@ export class PagerTargetComponent extends PluginComponent implements OnInit {
 	}
 
 	setPage(page: number) {
-		const maxAllowed = this.total();
-
-		if (page > 0 && page <= maxAllowed) {
-			this.model.pagination({current: page - 1});
-		}
+		this.model.pagination({current: page - 1});
 	}
 
-	attachEventListener() {
-		const input: HTMLInputElement = document.querySelector('#q-grid-menu-input');
+	handleKeyDown(event: KeyboardEvent) {
+		event.preventDefault();
 
-		input.addEventListener('keydown', (event: KeyboardEvent) => {
-			event.preventDefault();
+		const isEnter = event.key === 'Enter';
+		const key = Number.parseInt(event.key) || 0;
+		const total = this.total();
 
-			const isEnter = event.key === 'Enter';
-			const key = Number.parseInt(event.key) || 0;
-			const total = this.total();
-
-			if (isEnter) {
-				this.setPage(this.value);
-			} else if (key <= total && key >= 1) {
-				setTimeout(() => this.value = key);
-			} else {
-				setTimeout(() => this.value = '');
-			}
-		});
+		if (isEnter) {
+			this.setPage(this.value);
+		} else if (key <= total && key >= 1) {
+			setTimeout(() => this.value = key);
+		} else {
+			setTimeout(() => this.value = '');
+		}
 	}
 
 	total() {
