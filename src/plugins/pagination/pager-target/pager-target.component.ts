fd48733667e863c2fb01d0ac5511ccb3846b81e5
Andrey <munkk@yandex.ru>
2018-03-13 14:42:03 +0300

    Fixed input constraint issue

--- src/plugins/pagination/pager-target/pager-target.component.ts
+++ src/plugins/pagination/pager-target/pager-target.component.ts
@@ -1,17 +1,22 @@
-import { Component, Optional, OnInit } from '@angular/core';
+import {
+	Component, Optional, OnInit, HostListener, ViewChild, ElementRef, AfterViewInit,
+	ChangeDetectorRef
+} from '@angular/core';
 import { PluginComponent } from '../../plugin.component';
 import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
+import {Observable} from 'rxjs/Observable';
+
 
 @Component({
 	selector: 'q-grid-pager-target',
 	templateUrl: './pager-target.component.html'
 })
 export class PagerTargetComponent extends PluginComponent implements OnInit {
-	constructor(@Optional() root: RootService) {
+	constructor(@Optional() root: RootService, private cdRef: ChangeDetectorRef) {
 		super(root);
 	}
 
-	private value: number;
+	private value: any;
 	private target: any;
 
 	ngOnInit() {
@@ -24,9 +29,27 @@ export class PagerTargetComponent extends PluginComponent implements OnInit {
 
 		if (page > 0 && page <= maxAllowed) {
 			this.model.pagination({current: page - 1});
-		} else {
-			this.value = 1;
 		}
+	}
+
+	attachEventListener() {
+		const input: HTMLInputElement = document.querySelector('#q-grid-menu-input');
+
+		input.addEventListener('keydown', (event: KeyboardEvent) => {
+			event.preventDefault();
+
+			const isEnter = event.key === 'Enter';
+			const key = Number.parseInt(event.key) || 0;
+			const total = this.total();
+
+			if (isEnter) {
+				this.setPage(this.value);
+			} else if (key <= total && key >= 1) {
+				setTimeout(() => this.value = key);
+			} else {
+				setTimeout(() => this.value = '');
+			}
+		});
 	}
 
 	total() {
