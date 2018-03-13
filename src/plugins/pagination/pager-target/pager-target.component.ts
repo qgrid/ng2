ed7309ee18fd6f5db594e1d7a315f076d386bd16
Andrey <munkk@yandex.ru>
2018-03-13 20:11:27 +0300

    Fix styles and pager-target.component

--- src/plugins/pagination/pager-target/pager-target.component.ts
+++ src/plugins/pagination/pager-target/pager-target.component.ts
@@ -1,42 +1,33 @@
-import { Component, Optional, OnInit } from '@angular/core';
+import { Component, Optional } from '@angular/core';
 import { PluginComponent } from '../../plugin.component';
 import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
+import { Shortcut } from 'ng2-qgrid/core/shortcut/shortcut';
 
 @Component({
 	selector: 'q-grid-pager-target',
 	templateUrl: './pager-target.component.html'
 })
 
-export class PagerTargetComponent extends PluginComponent implements OnInit {
+export class PagerTargetComponent extends PluginComponent {
 	constructor(@Optional() root: RootService) {
 		super(root);
 	}
 
 	private value: any;
-	private target: any;
 
-	ngOnInit() {
-		this.target = this;
-		this.context = {$implicit: this.target};
-	}
+	keyDown(e: KeyboardEvent) {
+		e.preventDefault();
 
-	setPage(page: number) {
-		this.model.pagination({current: page - 1});
-	}
-
-	handleKeyDown(event: KeyboardEvent) {
-		event.preventDefault();
-
-		const isEnter = event.key === 'Enter';
-		const key = Number.parseInt(event.key) || 0;
+		const keyString = Shortcut.translate(e);
+		const keyNumber = Number.parseInt(keyString) || 0;
 		const total = this.total();
 
-		if (isEnter) {
-			this.setPage(this.value);
-		} else if (key <= total && key >= 1) {
-			setTimeout(() => this.value = key);
+		if (keyString === 'enter') {
+			this.model.pagination({current: this.value - 1});
+		} else if (keyNumber <= total && keyNumber >= 1) {
+			this.value = keyNumber;
 		} else {
-			setTimeout(() => this.value = 1);
+			this.value = 1;
 		}
 	}
 
