6d7b2e595345b80cc16c3d1f204c38c7b8beed2b
Andrey <munkk@yandex.ru>
2018-03-28 15:47:26 +0300

    ref. status-bar

--- src/plugins/status-bar/status-bar.component.ts
+++ src/plugins/status-bar/status-bar.component.ts
@@ -1,37 +1,25 @@
-import {AfterViewInit, Component, HostListener, Input, OnInit, Optional} from '@angular/core';
-import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
-import { PluginComponent } from '../plugin.component';
-import { Shortcut } from 'ng2-qgrid/core/shortcut/shortcut';
+import {Component, OnInit, Optional} from '@angular/core';
+import {RootService} from 'ng2-qgrid/infrastructure/component/root.service';
+import {PluginComponent} from '../plugin.component';
 
 @Component({
 	selector: 'q-grid-status-bar',
 	templateUrl: './status-bar.component.html'
 })
-export class StatusBarComponent extends PluginComponent {
-
+export class StatusBarComponent extends PluginComponent implements OnInit {
 	constructor(@Optional() root: RootService) {
 		super(root);
 	}
 
-	private rowIndex = 0;
-	private columnIndex = 0;
+	private rowIndex;
+	private columnIndex;
 
-	@HostListener('document:mouseup', ['$event']) mouseUp(event) {
-		this.cellLocation(event);
-	}
+	ngOnInit() {
+		this.using(this.model.navigationChanged.on(e => {
+			const newValue = e.changes.cell.newValue;
 
-	@HostListener('document:keyup', ['$event']) keyUp(event) {
-		this.cellLocation(event);
-	}
-
-	cellLocation(e) {
-		const isTableBody = e.currentTarget.activeElement.localName === 'tbody';
-
-		if (isTableBody) {
-			const navigation = this.model.navigation();
-
-			this.rowIndex = navigation.cell.rowIndex;
-			this.columnIndex = navigation.cell.columnIndex;
-		}
+			this.rowIndex = newValue.rowIndex;
+			this.columnIndex = newValue.columnIndex;
+		}));
 	}
 }
