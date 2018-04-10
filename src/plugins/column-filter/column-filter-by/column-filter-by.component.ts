f1d67c92199cedb9a264c18f706099987e10e458
Andrey <munkk@yandex.ru>
2018-04-10 18:41:16 +0300

    refactor.

--- src/plugins/column-filter/column-filter-by/column-filter-by.component.ts
+++ src/plugins/column-filter/column-filter-by/column-filter-by.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit, Optional} from '@angular/core';
+import { Component, Input, Optional } from '@angular/core';
 import { PluginComponent } from 'ng2-qgrid/plugins/plugin.component';
 import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
 
@@ -6,21 +6,20 @@ import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
 	selector: 'q-grid-column-filter-by',
 	templateUrl: './column-filter-by.component.html'
 })
-export class ColumnFilterByComponent extends PluginComponent implements OnInit {
+export class ColumnFilterByComponent extends PluginComponent {
 	@Input() public by: Set<string>;
-	@Input() public item: string;
-
-	private visibility;
 
 	constructor(@Optional() root: RootService) {
 		super(root);
 	}
 
-	ngOnInit() {
-		this.visibility = this.by.has(this.item);
+	isVisible(index: number): boolean {
+		return !this.by[index];
 	}
 
-	close(): void {
-		this.by.delete(this.item);
+	close(index: number): void {
+		const arr = Array.from(this.by);
+
+		this.by.delete(arr[index]);
 	}
 }
