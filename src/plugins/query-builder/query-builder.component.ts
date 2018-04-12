12f874019a34faf78b603c2d238f8ecaebaba700
klumba12 <dimitry.sitnov@gmail.com>
2018-04-12 20:19:13 +0300

    separate qb panel

--- src/plugins/query-builder/query-builder.component.ts
+++ src/plugins/query-builder/query-builder.component.ts
@@ -3,40 +3,31 @@ import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
 import { PluginComponent } from '../plugin.component';
 import { Node } from '../expression-builder/model/node';
 import { Command } from 'ng2-qgrid/core/command/command';
+import { Action } from 'ng2-qgrid/core/action/action';
+import { Composite } from 'ng2-qgrid/core/infrastructure/composite';
 
 @Component({
 	selector: 'q-grid-query-builder',
-	templateUrl: './query-builder.component.html'
+	template: ''
 })
 export class QueryBuilderComponent extends PluginComponent implements OnInit {
-	public node: Node;
-	@Output() close = new EventEmitter<any>();
-
-	submit = new Command({
-		source: 'query-builder.component',
-		execute: () => {
-
-		}
-	});
-
-	cancel = new Command({
-		source: 'query-builder.component',
-		execute: () => {
-
-		}
-	});
-
-	reset = new Command({
-		source: 'query-builder.component',
-		execute: () => {
-		}
-	});
-
 	constructor(@Optional() root: RootService) {
 		super(root);
 	}
 
 	ngOnInit() {
 		super.ngOnInit();
+
+		const actions = [
+			new Action(
+				new Command(),
+				'Query Builder',
+				'filter'
+			)
+		];
+
+		this.model.action({
+			items: Composite.list([actions, this.model.action().items])
+		});
 	}
 }
