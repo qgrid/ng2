92d3363b8e6264f2ed08f701433d1bbec4221cea
klumba12 <dimitry.sitnov@gmail.com>
2018-04-16 14:27:48 +0300

    formatting

--- src/plugins/expression-builder/eb-expression.component.ts
+++ src/plugins/expression-builder/eb-expression.component.ts
@@ -1,7 +1,7 @@
-import {Component, DoCheck, Input, OnInit} from '@angular/core';
-import {Line} from './model/line';
-import {Node} from './model/node';
-import {Watcher} from './watch';
+import { Component, DoCheck, Input, OnInit } from '@angular/core';
+import { Line } from './model/line';
+import { Node } from './model/node';
+import { Watcher } from './digest/watch';
 
 @Component({
 	selector: 'q-grid-eb-expression',
@@ -16,13 +16,31 @@ export class EbExpressionComponent implements OnInit, DoCheck {
 	context: any;
 
 	ngOnInit() {
-		const $watch = this.model.$watch || {};
+		this.context = { $implicit: this.model, node: this.node, line: this.line };
 
-		this.context = {$implicit: this.model, node: this.node, line: this.line};
-		this.watchers = Object.keys($watch).map(key => new Watcher(this.model, key, $watch[key], [this.node, this.line]));
+		const $watch = this.model.$watch;
+		if ($watch) {
+			this.watchers =
+				Object
+					.keys($watch)
+					.map(key =>
+						new Watcher(
+							this.model,
+							key,
+							$watch[key],
+							[this.node, this.line])
+					);
+		}
 	}
 
 	ngDoCheck() {
-		this.watchers.forEach(w => w.detect());
+		const ws = this.watchers;
+		if (!ws) {
+			return;
+		}
+
+		for (let i = 0, length = ws.length; i < length; i++) {
+			ws[i].detect();
+		}
 	}
 }
