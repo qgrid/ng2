e90da987ef73ddac199d589d5d5ebe19b88f9c1a
valentin.korolev <valentin.korolev@arcadia.spb.ru>
2018-04-16 14:05:41 +0300

    Add watchers to expressions

--- src/plugins/expression-builder/eb-expression.component.ts
+++ src/plugins/expression-builder/eb-expression.component.ts
@@ -1,12 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Line } from './model/line';
-import { Node } from './model/node';
+import {Component, DoCheck, Input, OnInit} from '@angular/core';
+import {Line} from './model/line';
+import {Node} from './model/node';
+import {Watcher} from './watch';
 
 @Component({
 	selector: 'q-grid-eb-expression',
 	templateUrl: './eb-expression.component.html'
 })
-export class EbExpressionComponent implements OnInit {
+export class EbExpressionComponent implements OnInit, DoCheck {
+	private watchers: Watcher[];
 	@Input() public node: Node;
 	@Input() public line: Line;
 	@Input() public model: any;
@@ -14,6 +16,13 @@ export class EbExpressionComponent implements OnInit {
 	context: any;
 
 	ngOnInit() {
-		this.context = { $implicit: this.model };
+		const $watch = this.model.$watch || {};
+
+		this.context = {$implicit: this.model, node: this.node, line: this.line};
+		this.watchers = Object.keys($watch).map(key => new Watcher(this.model, key, $watch[key], [this.node, this.line]));
+	}
+
+	ngDoCheck() {
+		this.watchers.forEach(w => w.detect());
 	}
 }
