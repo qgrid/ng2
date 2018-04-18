4a433b56d461a39ff8e66c9cff6ea0c588d02da4
valentin.korolev <valentin.korolev@arcadia.spb.ru>
2018-04-18 19:10:29 +0300

    Fix autosize directive.

--- src/common/resize/autosize.directive.ts
+++ src/common/resize/autosize.directive.ts
@@ -1,40 +1,48 @@
-import {Directive, ElementRef, OnInit, Input, Renderer2, NgZone} from '@angular/core';
+import {Directive, ElementRef, OnInit, Input, Renderer2, NgZone, DoCheck} from '@angular/core';
 import {Fastdom} from 'ng2-qgrid/core/services/fastdom';
+import {isUndefined} from 'ng2-qgrid/core/utility/index';
 
 @Directive({
 	selector: '[q-grid-autosize]'
 })
-export class AutosizeDirective implements OnInit {
+export class AutosizeDirective implements OnInit, DoCheck {
 	@Input('q-grid-autosize') selector;
 	@Input('q-grid-autosize-empty-width') emptyWidth = 75;
-	private actualText = '';
+	private actualText: string;
+	private host: HTMLElement;
+	private element: HTMLInputElement;
 
-	constructor(private element: ElementRef, private renderer: Renderer2, private zone: NgZone) {
+	constructor(element: ElementRef, private renderer: Renderer2, private zone: NgZone) {
+		this.host = element.nativeElement as HTMLInputElement;
 	}
 
 	ngOnInit() {
+		this.element = this.selector ? this.host.querySelector(this.selector) as HTMLInputElement : this.host as HTMLInputElement;
 		this.zone.runOutsideAngular(() => {
-			const owner = this.element.nativeElement as HTMLInputElement;
-			const element = this.selector ? owner.querySelector(this.selector) as HTMLInputElement : owner;
-
-			this.renderer.listen(element, 'input', () => this.autoWidth());
+			this.renderer.listen(this.element, 'input', () => this.autoWidth());
 		});
 	}
 
+	ngDoCheck() {
+		if (isUndefined(this.actualText)) {
+			if (this.element.value) {
+				this.autoWidth();
+			}
+		}
+	}
+
 	autoWidth() {
-		const owner = this.element.nativeElement as HTMLInputElement;
-		const element = this.selector ? owner.querySelector(this.selector) as HTMLInputElement : owner;
-		const text = element.value;
+		const text = this.element.value;
 
 		if (!text) {
 			this.actualText = text;
 			Fastdom.mutate(() => {
-				owner.style.width = `${this.emptyWidth}px`;
+				this.host.style.width = `${this.emptyWidth}px`;
 			});
 			return;
 		}
 
-		if (!element) {
+		if (!this.element) {
 			return;
 		}
 
@@ -44,8 +52,8 @@ export class AutosizeDirective implements OnInit {
 
 		this.actualText = text;
 		Fastdom.measure(() => {
-			const width = `${this.calculateWidth(element, text)}px`;
-			Fastdom.mutate(() => owner.style.width = width);
+			const width = `${this.calculateWidth(this.element, text)}px`;
+			Fastdom.mutate(() => this.host.style.width = width);
 		});
 	}
 
@@ -65,7 +73,7 @@ export class AutosizeDirective implements OnInit {
 			test.style.border = element.style.border;
 			// borderBox ?
 
-			document.body.appendChild(test);
+			body.appendChild(test);
 			width = test.offsetWidth;
 			test.remove();
 		}
