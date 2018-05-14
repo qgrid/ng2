b7817bde69f8c8141a5df3e1b81b2d5ac9c4909e
Andrey <munkk@yandex.ru>
2018-05-14 19:01:02 +0300

    add two way binding

--- src/plugins/column-filter/column-filter-by.component.ts
+++ src/plugins/column-filter/column-filter-by.component.ts
@@ -1,29 +1,32 @@
-import {Component, Input, Optional} from '@angular/core';
-import {PluginComponent} from 'ng2-qgrid/plugins/plugin.component';
-import {RootService} from 'ng2-qgrid/infrastructure/component/root.service';
-import {ColumnModel} from 'ng2-qgrid/core/column-type/column.model';
+import { Component, EventEmitter, Input, Optional, Output } from '@angular/core';
+import { PluginComponent } from 'ng2-qgrid/plugins/plugin.component';
+import { RootService } from 'ng2-qgrid/infrastructure/component/root.service';
+import { ColumnModel } from 'ng2-qgrid/core/column-type/column.model';
 
 @Component({
 	selector: 'q-grid-column-filter-by',
 	templateUrl: './column-filter-by.component.html'
 })
 export class ColumnFilterByComponent extends PluginComponent {
+	@Input() by: Set<string>;
 	@Input() column: ColumnModel;
-	@Input() columnFilter: any;
+	@Input() byBlanks: boolean;
+
+	@Output() byBlanksChange = new EventEmitter();
 
 	constructor(@Optional() root: RootService) {
 		super(root);
 	}
 
 	get isBlanks() {
-		return this.columnFilter.byBlanks;
+		return this.byBlanks;
 	}
 
 	remove(item: string): void {
 		if (item) {
-			this.columnFilter.by.delete(item);
+			this.by.delete(item);
 		} else {
-			this.columnFilter.byBlanks = false;
+			this.byBlanksChange.emit(false);
 		}
 	}
 }
