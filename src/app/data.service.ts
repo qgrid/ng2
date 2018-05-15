4044b5157e6bf4fec44f353ddc570af904fa69a8
klumba12 <dimitry.sitnov@gmail.com>
2018-05-15 18:35:42 +0300

    load people json

---
+++ src/app/data.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export class Human {
+	id: number;
+	contact: { email: string[] };
+	password: string;
+	comment: string;
+}
+
+@Injectable()
+export class DataService {
+	constructor(private http: HttpClient) {
+	}
+
+	getPeople(count: number): Observable<Human[]> {
+		return this.http.get<Human[]>(`assets/people/${count}.json`);
+	}
+
+	getPresets(): Observable<any> {
+		return this.http.get('assets/presets/1.json');
+	}
+}
