ad1c9400db358b4abc612db1999b6736e9105c6a
Andrey <munkk@yandex.ru>
2018-05-17 15:58:23 +0300

    Batch fix

--- src/core/edit/edit.service.js
+++ src/core/edit/edit.service.js
@@ -3,43 +3,46 @@ import {EditCellView} from './edit.cell.view';
 import {CommandManager} from '../command/command.manager';
 
 export class EditService {
-	constructor(model, table) {
-		this.model = model;
-		this.table = table;
-	}
+  constructor(model, table) {
+    this.model = model;
+    this.table = table;
+  }
 
-	doBatch(startCell) {
-		const label = startCell.label;
-		const value = startCell.value;
-		const editView = new EditCellView(this.model, this.table, new CommandManager());
-		try {
-			const startColumnType = startCell.column.type;
-			const columnIndices = this.model.columnList().index;
-			const selectionItems = this.model.selection().items;
+  doBatch(startCell) {
+    const view = this.model.view();
+    const editView = new EditCellView(this.model, this.table, new CommandManager());
 
-			for (let i = 0, max = selectionItems.length; i < max; i++) {
-				const {row, column} = selectionItems[i];
-				const key = column.key;
-				const columnIndex = columnIndices.indexOf(key);
-				const rowIndex = row.id;
-				const cellView = this.table.body.cell(rowIndex, columnIndex).model();
+    const label = startCell.label;
+    const value = startCell.value;
 
-				const cell = cellView.model;
-				const type = cell.column.type;
-				if (startColumnType === type) {
-					const editor = new CellEditor(cell);
-					editor.label = label;
-					editor.value = value;
-					editView.editor = editor;
+    const rows = view.rows;
+    const columns = view.columns;
+    try {
+      const startColumnType = startCell.column.type;
+      const selectionItems = this.model.selection().items;
 
-					if (editView.batchCommit.canExecute()) {
-						editView.batchCommit.execute();
-					}
-				}
-			}
-		}
-		finally {
-			editView.dispose();
-		}
-	}
+      for (let i = 0, max = selectionItems.length; i < max; i++) {
+        const {row, column} = selectionItems[i];
+        const rowIndex = rows.indexOf(row);
+        const columnIndex = columns.indexOf(column);
+
+        const cellView = this.table.body.cell(rowIndex, columnIndex).model();
+        const cell = cellView.model;
+        const type = cell.column.type;
+        if (startColumnType === type) {
+          const editor = new CellEditor(cell);
+          editor.label = label;
+          editor.value = value;
+          editView.editor = editor;
+
+          if (editView.batchCommit.canExecute()) {
+            editView.batchCommit.execute();
+          }
+        }
+      }
+    }
+    finally {
+      editView.dispose();
+    }
+  }
 }
