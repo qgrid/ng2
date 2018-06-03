5e11eaee9078c4b2da8e8bb99f7b030b8393339d
Andrey <munkk@yandex.ru>
2018-06-03 17:46:34 +0300

    Fix columnlist singleton issue

--- src/lib/main/column/column-list.service.ts
+++ src/lib/main/column/column-list.service.ts
@@ -4,7 +4,6 @@ import { compile } from 'ng2-qgrid/core/services/path';
 import { isUndefined, clone, isObject, identity } from 'ng2-qgrid/core/utility/kit';
 import { parseFactory } from 'ng2-qgrid/core/services/convert';
 import { ColumnListCtrl } from 'ng2-qgrid/core/column-list/column.list.ctrl';
-import { Singleton } from '../../infrastructure/design/singleton';
 import { ColumnModel } from 'ng2-qgrid/core/column-type/column.model';
 
 @Injectable()
@@ -32,7 +31,6 @@ export class ColumnListService {
 		this.ctrl.register(column);
 	}
 
-	@Singleton()
 	get ctrl() {
 		const canCopy = (key: string, source, target) =>
 			target.hasOwnProperty(key) && !isUndefined(source[key]);
