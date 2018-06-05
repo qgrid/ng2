650f24346afa315318b0857b2180f264e2b57793
klumba12 <dimitry.sitnov@gmail.com>
2018-06-05 06:41:53 +0300

    fix cache strategy

--- src/core/scene/render/cache.strategy.js
+++ src/core/scene/render/cache.strategy.js
@@ -28,54 +28,50 @@ export class CacheStrategy {
             if (storage.has(key)) {
                 return storage.get(key);
             }
-    
+
             const value = strategy.colspan(row, column, rowIndex, columnIndex);
             storage.set(key, value);
             return value;
         };
-    
+
         this.rowspan = (row, column, rowIndex, columnIndex) => {
             const key = `rowspan-${rowIndex}x${column.model.key}`;
             if (storage.has(key)) {
                 return storage.get(key);
             }
-    
+
             const value = strategy.rowspan(row, column, rowIndex, columnIndex);
             storage.set(key, value);
             return value;
         };
-    
+
         this.columns = (row, pin, rowIndex) => {
             const key = `columns-${pin}-${rowIndex}`;
             if (storage.has(key)) {
                 return storage.get(key);
             }
-    
+
             const value = strategy.columns(row, pin, rowIndex);
             storage.set(key, value);
             return value;
         }
-    
+
         this.setValue = (row, column, value, rowIndex, columnIndex) => {
             return strategy.setValue(row, column, value, rowIndex, columnIndex);
         }
-    
+
         this.columnList = (pin = null) => {
             const key = `columnList-${pin}`;
             if (storage.has(key)) {
                 return storage.get(key);
             }
-    
+
             const value = strategy.columnList(pin);
             storage.set(key, value);
             return value;
         }
-    
-        model.sceneChanged.watch(e => {
-            if (e.hasChanges('status')) {
-                storage = new Map();
-            }
-        });
+
+        model.sceneChanged.watch(() => storage = new Map());
 
         model.dataChanged.watch(e => {
             if (e.hasChanges('isReadonly')) {
