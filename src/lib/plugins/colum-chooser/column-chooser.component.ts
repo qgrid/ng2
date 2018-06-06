f61b34cbff3e1079774011ea78c7d960ff442994
Azamat Kurbanaev <azamat.kurbanaev@arcadia.spb.ru>
2018-06-07 02:29:18 +0300

    Implemented

--- src/lib/plugins/colum-chooser/column-chooser.component.ts
+++ src/lib/plugins/colum-chooser/column-chooser.component.ts
@@ -3,15 +3,17 @@ import { RootService } from '../../infrastructure/component/root.service';
 import { PluginComponent } from '../plugin.component';
 import { ColumnChooserView } from 'ng2-qgrid/plugin/column-chooser/column.chooser.view';
 import { FocusAfterRender } from '../../common/focus/focus.service';
+import { ThemeService } from '../../template/theme.service';
 
 const ColumnChooserName = 'qGridColumnChooser';
 
 @Component({
 	selector: 'q-grid-column-chooser',
+	styleUrls: ['../../assets/index.scss', '../../theme/material/index.scss'],
 	templateUrl: './column-chooser.component.html',
 	providers: [FocusAfterRender]
 })
-export class ColumnChooserComponent extends PluginComponent implements OnInit {
+export class ColumnChooserComponent extends PluginComponent {
 	@Input('canAggregate') columnChooserCanAggregate: boolean;
 	@Output('submit') submitEvent = new EventEmitter<any>();
 	@Output('cancel') cancelEvent = new EventEmitter<any>();
@@ -19,19 +21,11 @@ export class ColumnChooserComponent extends PluginComponent implements OnInit {
 	constructor(
 		@Optional() root: RootService, 
 		focusAfterRender: FocusAfterRender,
+		private theme: ThemeService,
 		private element: ElementRef) {
 		super(root);
 
 		this.models = ['columnChooser'];
-	}
-
-	ngOnInit() {
-		const model = this.model;
-
-		const element = this.element.nativeElement;
-		// model.style().classList.forEach(cssClass => {
-		// 	element.classList.add(cssClass);
-		// });
 	}
 
 	onReady() {
@@ -44,5 +38,15 @@ export class ColumnChooserComponent extends PluginComponent implements OnInit {
 		columnChooser.cancelEvent.on(() => this.cancelEvent.emit());
 
 		this.context = { $implicit: columnChooser };
+
+		const model = this.model;
+
+		const element = this.element.nativeElement;
+		const overlayPane= element.parentElement.parentElement.parentElement;
+		
+		model.style().classList.forEach(cssClass => {
+			overlayPane.classList.add(cssClass);
+		});
+
 	}
 }
