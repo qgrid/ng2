847d9655ac3477580fd3b80d293bb78f8bbde71d
klumba12 <dimitry.sitnov@gmail.com>
2018-06-09 17:55:08 +0300

    add more reference column examples

--- src/examples/column-reference-basic/example-column-reference-basic.component.ts
+++ src/examples/column-reference-basic/example-column-reference-basic.component.ts
@@ -1,47 +1,69 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService, Atom } from '../data.service';
 import { Observable } from 'rxjs';
-import { Action, Command, Grid, GridModel, EditorOptions } from 'ng2-qgrid';
+import { Grid, EditorOptions } from 'ng2-qgrid';
 
 @Component({
 	selector: 'example-column-reference-basic',
 	templateUrl: 'example-column-reference-basic.component.html',
-	styleUrls: ['example-column-reference-basic.component.scss'],
-	providers: [DataService]
+	styleUrls: ['example-column-reference-basic.component.scss']
 })
 export class ExampleColumnReferenceBasicComponent {
-	rows: Observable<Atom[]>;
-	showSamePhaseAtomsOptions: EditorOptions = {
+	rows = [
+		{
+			'notEditable': ['Lorem', 'ipsum', 'dolor', 'sit', 'amet'],
+			'editable': ['Lorem', 'ipsum'],
+			'customTemplate': ['Lorem', 'dolor', 'amet']
+		}
+	];
+
+	notEditableOptions: EditorOptions = {
 		modelFactory: ({ row }) => {
 			const model = this.qgrid.model();
-			this.rows.subscribe(atoms => {
-				model
-					.data({
-						rows: atoms.filter(atom => atom.phase === row.phase),
-						columns: [{
-							key: 'symbol',
-							title: 'Symbol',
-							canFilter: false
-						}, {
-							key: 'name',
-							title: 'Name',
-							canFilter: false
-						}]
-					})
-					.visibility({
-						toolbar: {
-							top: false,
-							bottom: false,
-							right: false,
-							left: false
+			model
+				.data({
+					rows: this.rows[0].notEditable.map(value => ({ value })),
+					columns: [
+						{
+							key: 'value',
+							title: 'Not Editable'
 						}
-					});
-			});
+					]
+				})
+				.visibility({
+					toolbar: {
+						top: false,
+						bottom: false,
+						right: false,
+						left: false
+					}
+				});
+
 			return model;
 		}
 	};
 
-	constructor(dataService: DataService, private qgrid: Grid) {
-		this.rows = dataService.getAtoms();
+	editableOptions: EditorOptions = {
+		modelFactory: ({ row }) => {
+			const model = this.qgrid.model();
+			model
+				.data({
+					rows: this.rows[0].notEditable.map(value => ({ value })),
+					columns: [
+						{
+							key: 'value',
+							title: 'Editable'
+						}
+					]
+				})
+				.selection({
+					unit: 'row',
+					mode: 'multiple'
+				});
+
+			return model;
+		}
+	};
+
+	constructor(private qgrid: Grid) {
 	}
 }
