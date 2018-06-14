e1f13a5b95609c97de7c12a0acb08e03ad5bf38b
klumba12 <dimitry.sitnov@gmail.com>
2018-06-14 13:09:44 +0300

    get rid of browser module

--- src/lib/plugins/persistence/persistence.module.ts
+++ src/lib/plugins/persistence/persistence.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { PersistenceComponent } from './persistence.component';
 import { PersistencePanelComponent } from './persistence-panel.component';
@@ -7,7 +6,6 @@ import { TemplateModule } from '../../template/template.module';
 
 @NgModule({
 	imports: [
-		BrowserModule,
 		FormsModule,
 		TemplateModule
 	],
