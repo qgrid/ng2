6340427001bf4724e6f73be7f95601f8e5d9d7a8
Ekaterina Tokareva <Tokaree@labcorp.com>
2018-06-26 19:52:10 +0300

    add interface info

--- src/core/column-list/column.list.generate.d.ts
+++ src/core/column-list/column.list.generate.d.ts
@@ -3,3 +3,5 @@ import { Model } from '../infrastructure/model';
 
 export declare function generateFactory(model: Model):
 	(rows: any[], columnFactory: (type: string) => ColumnModel, deep: boolean) => ColumnModel[];
+
+export declare function generate(settings: any): ColumnModel[];
