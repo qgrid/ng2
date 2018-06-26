0242a6eeff9e6ea47cf386f61b836261fd04dbb8
Ekaterina Tokareva <Tokaree@labcorp.com>
2018-06-26 16:20:06 +0300

    export component

--- src/lib/plugins/export/export.component.ts
+++ src/lib/plugins/export/export.component.ts
@@ -1,42 +1,47 @@
-import { Component, Input, OnInit, Optional } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, Input, TemplateRef } from '@angular/core';
 import { PluginService } from '../plugin.service';
+import { ColumnModel } from 'ng2-qgrid/core/column-type/column.model';
 import { Command } from 'ng2-qgrid/core/command/command';
 import { ExportView } from 'ng2-qgrid/plugin/export/export.view';
-import { ColumnModel } from 'ng2-qgrid/core/column-type/column.model';
-import { Xlsx } from './xlsx';
-import { Pdf } from './pdf';
-import { downloadFactory } from './download';
-import { TemplateHostService } from '../../template/template-host.service';
+import { Xlsx } from 'ng2-qgrid/plugin/export/xlsx';
+import { Pdf } from 'ng2-qgrid/plugin/export/pdf';
+import { downloadFactory } from 'ng2-qgrid/plugin/export/download';
 import { Action } from 'ng2-qgrid/core/action/action';
 import { Composite } from 'ng2-qgrid/core/infrastructure/composite';
+import { TemplateHostService } from '../../template/template-host.service';
 
 @Component({
 	selector: 'q-grid-export',
 	templateUrl: './export.component.html',
 	providers: [ TemplateHostService, PluginService ]
 })
-export class ExportComponent implements OnInit {
+export class ExportComponent implements AfterViewInit {
 	@Input() type: string;
+	@ContentChild(TemplateRef) templateRef: TemplateRef<any>;
 
 	context: { $implicit: ExportComponent } = {
 		$implicit: this
 	};
 
 	constructor(private plugin: PluginService, private templateHost: TemplateHostService) {
+		this.templateHost.key = () => `export-${this.type}`;
 	}
 
-	ngOnInit() {
-		this.templateHost.key = (source: string) => `${source}-${this.templateHostKey}`;
+	ngAfterViewInit() {
 		const { model } = this.plugin;
 		const exportView = new ExportView(model, { type: this.type });
 		const action = new Action(
-			new Command({ execute: () => exportView[this.type].execute() }),
+			new Command({ execute: () => exportView[ this.type ].execute() }),
 			`Export to ${this.type}`,
 			'file_download'
 		);
 
+		if (this.templateRef) {
+			action.templateUrl = this.templateHost.key('trigger');
+		}
+
 		model.action({
-			items: Composite.list([ [ action ], model.action().items ])
+			items: Composite.list([ model.action().items, [ action ] ])
 		}, {
 			source: 'export.component'
 		});
@@ -53,12 +58,5 @@ export class ExportComponent implements OnInit {
 	get id() {
 		return this.plugin.model.grid().id;
 	}
-
-	get templateContentKey() {
-		return [ `content-${this.templateHostKey}`, 'plugin-export.tpl.html' ];
-	}
-
-	get templateHostKey() {
-		return `plugin-export-${this.type}.tpl.html`;
-	}
 }
+
