0441a2a220f75e06e6bd54bba87131b4c050ac9e
Ekaterina Tokareva <Tokaree@labcorp.com>
2018-06-30 11:33:27 +0300

    refactoring of json write

--- src/core/export/json/json.js
+++ src/core/export/json/json.js
@@ -1,25 +1,18 @@
-import {flatView} from '../export.service';
-
-function updateTitles(rows, columns) {
-	const result = [];
-	for (let row of rows) {
-		const obj = {};
-		for (let column of columns) {
-			obj[column.title] = row[column.key];
-		}
-		result.push(obj);
-	}
-	return JSON.stringify(result, '', 4);
-}
+import { flatView } from '../export.service';
 
 export class Json {
 	write(rows, columns) {
 		const result = [];
 
 		for (let row of rows) {
-			result.push(flatView(row));
+			const rowObj = flatView(row);
+			const obj = {};
+			for (let column of columns) {
+				obj[ column.title ] = rowObj[ column.key ];
+			}
+			result.push(obj);
 		}
 
-		return updateTitles(result, columns);
+		return JSON.stringify(result, '', 4);
 	}
 }
