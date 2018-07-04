88df14f0dc65440767fbc4117296a4b6a6405b80
Max Kilovatiy <maxkvt@gmail.com>
2018-07-04 14:39:13 +0300

    Add commit cancel support

--- src/lib/plugins/edit-form/edit-form.component.ts
+++ src/lib/plugins/edit-form/edit-form.component.ts
@@ -2,9 +2,11 @@ import { Component, Optional, Input, OnInit, OnDestroy } from '@angular/core';
 import { EditFormView } from 'ng2-qgrid/plugin/edit-form/edit.form.view';
 import { FormGroup } from '@angular/forms';
 import { ColumnModel } from 'ng2-qgrid/core/column-type/column.model';
-import { RowEditor } from 'ng2-qgrid/core/edit/edit.row.editor';
+// import { RowEditor } from 'ng2-qgrid/core/edit/edit.row.editor';
+import { EditFormPanelView } from 'ng2-qgrid/plugin/edit-form/edit.form.panel.view';
 import { CellEditor } from 'ng2-qgrid/core/edit/edit.cell.editor';
 import { PluginService } from '../plugin.service';
+import { ViewCoreService } from '../../main/core/view/view-core.service';
 
 @Component({
 	selector: 'q-grid-edit-form',
@@ -15,7 +17,9 @@ export class EditFormComponent implements OnInit, OnDestroy {
 	@Input() title: string;
 	@Input() cell: any;
 
+	private panelView: any;
 	private editors: CellEditor[];
+	private editor: any;
 
 	context: { $implicit: EditFormComponent } = {
 		$implicit: this
@@ -25,9 +29,9 @@ export class EditFormComponent implements OnInit, OnDestroy {
 	}
 
 	ngOnInit() {
-		const { line } = this.plugin.model.columnList();
-		const editor = new RowEditor(this.cell.row, line);
-		this.editors = editor.editors;
+		this.panelView = new EditFormPanelView(this.plugin.model, this.cell);
+		this.editor = this.panelView.editor;
+		this.editors = this.panelView.editor.editors;
 	}
 
 	ngOnDestroy() {
@@ -41,5 +45,13 @@ export class EditFormComponent implements OnInit, OnDestroy {
 		}
 
 		return 'edit-form-default.tpl.html';
+	}
+
+	save(event: any): void {
+		this.panelView.submit.execute();
+	}
+
+	cancel(event: any): void {
+		this.panelView.cancel.execute();
 	}
 }
