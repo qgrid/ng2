8a8dd533de5334024980468877415af4cc6371a8
klumba12 <dimitry.sitnov@gmail.com>
2018-07-23 18:33:11 +0300

    propogate events from form editor

--- src/lib/plugins/edit-form/edit-form.component.ts
+++ src/lib/plugins/edit-form/edit-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, EventEmitter, Output } from '@angular/core';
 import { EditFormPanelView } from 'ng2-qgrid/plugin/edit-form/edit.form.panel.view';
 import { PluginService } from '../plugin.service';
 import { DomTd } from 'ng2-qgrid';
@@ -12,14 +12,22 @@ export class EditFormComponent implements OnInit, OnDestroy {
 	@Input() caption: string;
 	@Input() cell: DomTd;
 
+	@Output() cancel = new EventEmitter();
+	@Output() reset = new EventEmitter();
+	@Output() submit = new EventEmitter();
+
 	context: { $implicit: EditFormPanelView };
 
 	constructor(private plugin: PluginService) {
 	}
 
 	ngOnInit() {
-		const $implicit = new EditFormPanelView(this.plugin.model, { row: this.cell.row, caption: this.caption });
-		this.context = { $implicit };
+		const view = new EditFormPanelView(this.plugin.model, { row: this.cell.row, caption: this.caption });
+		view.submitEvent.on(() => this.submit.emit());
+		view.cancelEvent.on(() => this.cancel.emit());
+		view.resetEvent.on(() => this.reset.emit());
+
+		this.context = { $implicit: view };
 	}
 
 	ngOnDestroy() {
