273e09d4cf2aa0c55738388ca2f2a2536607b7f5
klumba12 <dimitry.sitnov@gmail.com>
2018-08-15 18:34:30 +0300

    Better specifications

--- e2e/steps/pagination-basic.steps.ts
+++ e2e/steps/pagination-basic.steps.ts
@@ -4,9 +4,17 @@ import { element, by, browser, ExpectedConditions as until } from 'protractor';
 When('I choose page size {int}', size => openPageSizeSelect().then(() => selectPageSizeOption(size)));
 
 function openPageSizeSelect() {
-    return element(by.css('.q-grid-page-size')).click().then(() => browser.wait(until.presenceOf(element(by.css('.mat-option-text'))), 5000));
+	return element(by.css('mat-select.q-grid-page-size'))
+		.click()
+		.then(() =>
+			browser.wait(
+				until.presenceOf(
+					element(by.css('.mat-option-text'))
+				),
+				5000));
 }
 
+
 function selectPageSizeOption(size) {
-    return element(by.cssContainingText('.mat-option-text', '' + size)).click();
+	return element(by.cssContainingText('.mat-option-text', '' + size)).click();
 }
