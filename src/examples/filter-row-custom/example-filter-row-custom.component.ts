e4951973ce662adf5bf648f44d8f34a7a0875534
Yury Filatov <jurgenfilatov@gmail.com>
2018-10-31 11:16:20 +0300

    Two filter (name, phase)

--- src/examples/filter-row-custom/example-filter-row-custom.component.ts
+++ src/examples/filter-row-custom/example-filter-row-custom.component.ts
@@ -17,11 +17,37 @@ export class ExampleFilterRowCustomComponent {
 		this.rows = dataService.getAtoms();
 	}
 
-	filter(value: string) {
+	search = {
+		name: '',
+		phase: ''
+	  };
+	  
+
+	filter(name: string, value: string) {
+	this.search[name] = value;
+	const { model} = this.myGrid;
+
+	const predicate = Object
+		.keys(this.search)
+		.reduce((memo: (x: any) => boolean, key) => {
+		const searchText = this.search[key].toLowerCase();
+		if (searchText) {
+			return row => memo(row) && ('' + row[key]).toLowerCase().indexOf(searchText) >= 0;
+		}
+
+		return memo;
+		}, x => true);
+
+		model.filter({
+			match: () => predicate
+		});
+	}
+	
+	/* filter(value: string) {
 		const { model } = this.myGrid;
 		value = value.toLocaleLowerCase();
 		model.filter({
 			match: () => row => row.name.toLowerCase().indexOf(value) >= 0
 		});
-	}
+	} */
 }
