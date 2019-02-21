bdc2ae152a864c11d0e227942d8c1de00c12880f
klumba12 <dimitry.sitnov@gmail.com>
2019-02-21 16:14:28 +0300

    add key with symbols example

---
+++ src/examples/key-with-symbols/example-key-with-symbols.component.ts
@@ -0,0 +1,27 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+	selector: 'example-key-with-symbols',
+	templateUrl: 'example-key-with-symbols.component.html',
+	styleUrls: ['example-key-with-symbols.component.scss'],
+	providers: [DataService],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ExampleKeyWithSymbolsComponent {
+	rows = [{
+		'key.With.Symbols': {
+			'sub.Key.With."."': 'sub-key with dot',
+			'sub.Key.With."@"': 'sub-key with "@"',
+			'sub.Key.With.","': 'sub-key with ","',
+			'sub.Key.With."!"': 'sub-key with "!"',
+			'sub.Key.With."$"': 'sub-key with "$"',
+			'sub.Key.With."?"': 'sub-key with "?"',
+			'sub.Key.With."\'"': 'sub-key with "\'"',
+			'sub.Key.With."^"': 'sub-key with "^"',
+			'sub.Key.With.";"': 'sub-key with ";"',
+			'sub.Key.With."#"': 'sub-key with "#"',
+			'sub.Key.With."%"': 'sub-key with "%"',
+		}
+	}];
+}
