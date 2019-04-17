5725f321b00556932ca3b66bb7a9d8a5385245c3
Pitsik <julia.pitsik@arcadia.spb.ru>
2019-04-17 11:39:57 +0300

    refactoring of pipes

--- src/app/app.filter.pipe.ts
+++ src/app/app.filter.pipe.ts
@@ -6,13 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class FilterSearch implements PipeTransform {
     transform(item: string, search: string) {
-        if (item && search) {
             const contains = new RegExp(search, 'gi');
-            if (contains.test(item)) {
-                return true;
-            }
-        } else {
-            return true;
-        }
+            return (contains.test(item));
     }
 }
--- src/app/app.highlight.pipe.ts
+++ src/app/app.highlight.pipe.ts
@@ -6,13 +6,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class HighlightSearch implements PipeTransform {
     transform(item: string, search: string) {
-        if (search && item) {
-            const contains = new RegExp(search, 'gi');
-            if (contains.test(item)) {
-                return item.replace(contains, elem => `<span class='highlight'>${elem}</span>`);
-            }
-        } else {
-            return item;
-        }
+        const contains = new RegExp(search, 'gi');
+        return (contains.test(item) && search)
+            ? item.replace(contains, elem => `<span class='highlight'>${elem}</span>`)
+            : item;
     }
 }
