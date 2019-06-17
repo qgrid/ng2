bfb211cc9125047c1f971b9c4b3374692afd3b6f
klumba12 <dimitry.sitnov@gmail.com>
2019-06-17 17:56:22 +0300

    style adjustments

--- src/app/app.filter.pipe.ts
+++ src/app/app.filter.pipe.ts
@@ -3,7 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
 	name: 'filter'
 })
-
 export class FilterSearch implements PipeTransform {
 	transform(items: any[], search: string) {
 		const contains = new RegExp(search, 'i');
