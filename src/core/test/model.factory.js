96b493b196410cb48dcd6427c3a8ed53cd6796e8
klumba12 <dimitry.sitnov@gmail.com>
2019-06-19 14:08:26 +0300

    fix tests

--- src/core/test/model.factory.js
+++ src/core/test/model.factory.js
@@ -1,47 +1,6 @@
 import { ModelBuilder } from '../infrastructure/model.builder';
 
 const builder = new ModelBuilder();
-builder
-	.register('grid', GridModel)
-	.register('pipe', PipeModel)
-	.register('scene', SceneModel)
-	.register('view', ViewModel)
-	.register('data', DataModel)
-	.register('selection', SelectionModel)
-	.register('head', HeadModel)
-	.register('body', BodyModel)
-	.register('navigation', NavigationModel)
-	.register('focus', FocusModel)
-	.register('foot', FootModel)
-	.register('layout', LayoutModel)
-	.register('row', RowModel)
-	.register('columnList', ColumnListModel)
-	.register('rowList', RowListModel)
-	.register('sort', SortModel)
-	.register('group', GroupModel)
-	.register('pivot', PivotModel)
-	.register('edit', EditModel)
-	.register('plugin', PluginModel)
-	.register('toolbar', ToolbarModel)
-	.register('layer', LayerModel)
-	.register('pagination', PaginationModel)
-	.register('progress', ProgressModel)
-	.register('highlight', HighlightModel)
-	.register('visibility', VisibilityModel)
-	.register('filter', FilterModel)
-	.register('drag', DragModel)
-	.register('style', StyleModel)
-	.register('scroll', ScrollModel)
-	.register('export', ExportModel)
-	.register('import', ImportModel)
-	.register('action', ActionModel)
-	.register('fetch', FetchModel)
-	.register('persistence', PersistenceModel)
-	.register('validation', ValidationModel)
-	.register('template', TemplateModel)
-	.register('rest', RestModel)
-	.register('animation', AnimationModel);
-
 export function modelFactory() {
 	return builder.build();
 }
