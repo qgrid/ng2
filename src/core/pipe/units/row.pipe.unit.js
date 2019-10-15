c77d2900bc0f4771ffa0812a207ae207b7c2ead0
Julia Pitsik <julia.pitsik@arcadia.spb.ru>
2019-10-15 16:26:04 +0300

    add animation pipe for drag and drop

--- src/core/pipe/units/row.pipe.unit.js
+++ src/core/pipe/units/row.pipe.unit.js
@@ -1,15 +1,26 @@
 import { sortFactory } from '../../row-list/row.list.sort';
+import { Pipe } from '../pipe';
 
 export const rowPipeUnit = [
 	(_, context, next) => {
+		const { model } = context;
+		const order = sortFactory(model);
+		const rows = order(model.view().rows);
+		const memo = {
+			rows,
+			pivot: model.view().pivot,
+			nodes: model.view().nodes
+		};
+		next(memo);
+	},
+	Pipe.animation,
+	({ rows }, context, next) => {
+		const { model } = context;
 		const tag = {
 			source: context.source || 'row.pipe.unit',
 			behavior: 'core'
 		};
 
-		const { model } = context;
-		const order = sortFactory(model);
-		const rows = order(model.view().rows);
 		model.view({ rows }, tag);
 		model.scene({ rows }, tag);
 
