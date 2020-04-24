735ee73092aafcf05bd3a15b62912da2c5c9d510
klumba12 <dimitry.sitnov@gmail.com>
2020-04-24 17:55:17 +0300

    package prod.json

--- build.lib.js
+++ build.lib.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const { inImports, fixProjectPaths } = require('./build.import');
 const { buildLib, buildTheme } = require('./build.kit');
 const { relativeCopy } = require('./build.utils');
 
 const ROOT_PATH = path.resolve('.');
-
 const PROJECTS_PATH = path.join(ROOT_PATH, 'projects');
 const DIST_PATH = path.join(ROOT_PATH, 'dist');
 const NG2_DIST_PATH = path.join(DIST_PATH, 'ng2-qgrid');
@@ -43,8 +43,19 @@ async function main() {
   buildTheme('ng2-qgrid-theme-basic');
   await buildLib('ng2-qgrid-theme-basic', OPTIONS);
 
+  await relativeCopy('package.prod.json',
+    path.join(DIST_PATH, 'ngx-qgrid-plugins'),
+    path.join(NG2_DIST_PATH, '@qgrid', 'ngx-plugins')
+  );
+
   buildTheme('ng2-qgrid-theme-material');
   await buildLib('ng2-qgrid-theme-material', OPTIONS);
+
+  console.log('build.lib: copy package.json');
+  fs.copyFileSync(
+    path.join(PROJECTS_PATH, 'ng2-qgrid', 'package.prod.json'),
+    path.join(NG2_DIST_PATH, 'package.json')
+  );
 }
 
 main();
