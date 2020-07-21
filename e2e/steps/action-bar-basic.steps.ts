c4c8d9df487e37c1ca87dfc7d5be0616e24a769b
Sergey Shalygin <shalygins23@gmail.com>
2020-07-21 21:03:56 +0300

    two tests added

--- e2e/steps/action-bar-basic.steps.ts
+++ e2e/steps/action-bar-basic.steps.ts
@@ -1,9 +1,20 @@
 import { When } from 'cucumber';
-import { element, by } from 'protractor';
+import { element, by, browser } from 'protractor';
 
 When('I click load button', () => click('refresh'));
 When('I click clear button', () => click('clear_all'));
+When('I scroll page right [{int}]', async (num: number) => scrollElementHorizontalOnValue(await element.all(by.tagName('tbody')).get(0).getWebElement(), num));
+When('I scroll page left [{int}]', async (num: number) => scrollElementHorizontalOnValue(await element.all(by.tagName('tbody')).get(0).getWebElement(), -num));
+When('I sort column {string} downwards', async (col: string) => sortColumnDownwards(col));
 
 function click(text) {
 	return element(by.buttonText(text)).click();
+}
+
+async function scrollElementHorizontalOnValue(elem, crement) {
+    browser.executeScript('arguments[0].scrollLeft = arguments[0].scrollLeft + arguments[1]', elem, crement);
+}
+
+async function sortColumnDownwards(col) {
+	return element(by.xpath('//label[contains(text(), \'' + col + '\')]/../mat-icon[contains(text(), \'arrow_downward\')]')).click();
 }
