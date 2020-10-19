d773bf19d8e582021ab737cc74c58c709a7f0c99
klumba12 <dimitry.sitnov@gmail.com>
2020-10-19 11:03:33 +0300

    fix keyboard d.ts

--- projects/qgrid-core/keyboard/keyboard.d.ts
+++ projects/qgrid-core/keyboard/keyboard.d.ts
@@ -2,5 +2,5 @@ export declare class Keyboard {
 	static isPrintable(code: string): boolean;
 	static isControl(code: string): boolean;
 	static stringify(code: string, key: string): string;
-	static translate(code: string): string;
+	static translate(code: number): string;
 }
