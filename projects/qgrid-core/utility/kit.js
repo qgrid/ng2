6e7a09377fede4c82307e845705277fde3924d09
Daniil Dubrava <daniil.dubrava@yandex.ru>
2020-11-17 16:03:45 +0300

    feat(util/kit): handle cases when x or y is null

--- projects/qgrid-core/utility/kit.js
+++ projects/qgrid-core/utility/kit.js
@@ -48,15 +48,23 @@ const isEmail = value => {
 };
 
 function compare(x, y) {
-	if (x === y) {
-		return 0;
+	if (x === null) {
+		return -1;
+	}
+
+	if (y === null) {
+		return 1;
 	}
 
 	if (x > y) {
 		return 1;
 	}
 
-	return -1;
+	if (x < y) {
+		return -1;
+	}
+
+	return 0;
 }
 
 function orderBy(data, selectors, compares) {
