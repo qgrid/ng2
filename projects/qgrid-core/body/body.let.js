273cf419bcf1ea628667fc2017d7bb6a23c471b9
Dalantren <ivanita@labcorp.com>
2020-11-26 16:54:32 +0300

    added text selection on mouse right click

--- projects/qgrid-core/body/body.let.js
+++ projects/qgrid-core/body/body.let.js
@@ -1,14 +1,17 @@
+import { EventListener } from '../event/event.listener';
+import { EventManager } from '../event/event.manager';
 import { Log } from '../infrastructure/log';
 import { Renderer } from '../scene/render/render';
 
 export class BodyLet {
 	constructor(plugin) {
-		const { model, observe } = plugin;
+		const { model, observe, disposable } = plugin;
 		const render = new Renderer(plugin);
 
 		this.plugin = plugin;
 		this.render = render;
 		this.columns = pin => render.defaultStrategy.columnList(pin);
+		this.selectedNodes = [];
 
 		observe(model.sceneChanged)
 			.subscribe(e => {
@@ -16,6 +19,13 @@ export class BodyLet {
 					this.tryShowBlankLayer();
 				}
 			});
+		
+		const { view } = plugin.table.box.markup;
+		const manager = new EventManager(this);
+
+		disposable.add(new EventListener(view, manager).on('contextmenu', event => this.selectTextInNode(event.target)));
+		disposable.add(new EventListener(document, manager).on('click', this.removeSelections));
+		disposable.add(new EventListener(window, manager).on('blur', this.removeSelections));
 
 		this.tryShowBlankLayer();
 	}
@@ -35,5 +45,34 @@ export class BodyLet {
 				table.view.removeLayer('blank');
 			}
 		}
+	}
+
+	removeSelections() {
+		this.selectedNodes.forEach(this.removeSelection);
+		this.selectedNodes = [];
+	}
+
+	removeSelection(node) {
+		if (node && node.removeAttribute) {
+			node.removeAttribute('style');
+		}
+	}
+
+	selectTextInNode(node) {
+		node.setAttribute('style', 'user-select: auto;');
+		if (document.body.createTextRange) {
+			const range = document.body.createTextRange();
+			range.moveToElementText(node);
+			range.select();
+		} else if (window.getSelection) {
+			const selection = window.getSelection();
+			const range = document.createRange();
+			range.selectNodeContents(node);
+			selection.removeAllRanges();
+			selection.addRange(range);
+		} else {
+			console.warn("Could not select text in node: Unsupported browser.");
+		}
+		this.selectedNodes.push(node);
 	}
 }
