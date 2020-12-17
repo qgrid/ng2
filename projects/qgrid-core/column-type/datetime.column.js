76044c9f006d852d7704b235bdff52697f6318a8
Dalantren <ivanita@labcorp.com>
2020-12-17 14:27:54 +0300

    Replaced qGridDate pipes with cell label

--- projects/qgrid-core/column-type/datetime.column.js
+++ projects/qgrid-core/column-type/datetime.column.js
@@ -1,8 +1,9 @@
 import { ColumnView } from '../scene/view/column.view';
 import { DataColumnModel } from './data.column.model';
-import { TemplatePath } from '../template/template.path';
-import { get as getValue } from '../services/value';
 import { FormatService } from '../format/format.service';
+import { get as getValue } from '../services/value';
+import { getType, parseFactory } from '../services/convert';
+import { TemplatePath } from '../template/template.path';
 
 TemplatePath.register('datetime-cell', (template, column) => {
 	return {
@@ -25,11 +26,11 @@ export class DateTimeColumnModel extends DataColumnModel {
 		this.format = 'MM/dd/yyyy h:mm a';
 		this.dateFormat = 'MM/dd/yyyy';
 		this.timeFormat = 'h:mm a';
+		this.parse = parseFactory('datetime');
 
 		this.label = function (row) {
 			const value = getValue(row, this);
-			const parse = parseFactory('datetime');
-			const date = parse(value);
+			const date = this.parse(value);
 			const isValidDate = getType(date) === 'datetime' && !isNaN(date);
 			return isValidDate ? FormatService.date(date, this.format) : value; 
 		};
