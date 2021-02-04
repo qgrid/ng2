22d2339c2e654782740d57d0a8f75eac1df1d5da
Evgeniy Cheremukhin <evgeniy.cheremukhin@arcadia.spb.ru>
2021-02-04 13:37:26 +0300

    Use ngclass

--- src/examples/grid-size/example-grid-size.component.ts
+++ src/examples/grid-size/example-grid-size.component.ts
@@ -1,7 +1,6 @@
 import { Component, ChangeDetectionStrategy, ViewChild } from '@angular/core';
 import { DataService, Human } from '../data.service';
 import { Observable } from 'rxjs';
-import { GridComponent } from 'ng2-qgrid';
 
 @Component({
 	selector: 'example-grid-size',
@@ -9,43 +8,14 @@ import { GridComponent } from 'ng2-qgrid';
 	styleUrls: ['example-grid-size.component.scss'],
 	providers: [DataService],
 	changeDetection: ChangeDetectionStrategy.OnPush,
-	styles: [
-		`.plain{width:100%; height:100%;}
-		.medium{width:60%; height:60%  !important;}
-		.small{width:500px; height:500px  !important;}`
-	]
 })
 export class ExampleGridSizeComponent {
 	static id = 'grid-size';
 
 	rows: Observable<Human[]>;
-	@ViewChild(GridComponent, { static: true }) grid: GridComponent;
-
-	gridClass = {
-		plain: true,
-		medium: false,
-		small: false
-	}
+	sizeClass = 'fill';
 
 	constructor(dataService: DataService) {
 		this.rows = dataService.getPeople();
-	}
-
-	fillPlain() {
-		this.gridClass.plain = true;
-		this.gridClass.medium = false;
-		this.gridClass.small = false;
-	}
-
-	fillMedium() {
-		this.gridClass.plain = false;
-		this.gridClass.medium = true;
-		this.gridClass.small = false;
-	}
-
-	fillSmall() {
-		this.gridClass.plain = false;
-		this.gridClass.medium = false;
-		this.gridClass.small = true;
 	}
 }
