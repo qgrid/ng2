3ef62a87b3619e0cb4d8f3050ec4382fd4d255f4
Dmitry Sitnov <dimitry.sitnov@gmail.com>
2021-02-11 17:01:37 +0300

    fix document click command

--- projects/qgrid-core/command-bag/document.click.command.js
+++ projects/qgrid-core/command-bag/document.click.command.js
@@ -6,14 +6,15 @@ export class DocumentClickCommand extends Command {
     constructor(plugin) {
         const { model, commandPalette } = plugin;
 
+        const isPartOfGrid = (element, host) => element === host || (element.classList && element.classList.contains('q-grid-editor-part'));
+
         super({
             key: DOCUMENT_CLICK_COMMAND_KEY,
             execute: ([host, e]) => {
                 if (model.edit().status === 'edit') {
                     const path = eventPath(e);
-                    const clickedOutside = path.every(x => x !== host && !x.classList.contains('q-grid-editor-part'));
+                    const clickedOutside = !path.some(x => isPartOfGrid(x, host));
                     if (clickedOutside) {
-
                         const cancelEdit = commandPalette.get(EDIT_CELL_CANCEL_COMMAND_KEY);
                         if (cancelEdit.canExecute() === true) {
                             cancelEdit.execute();
