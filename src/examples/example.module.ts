7ee00c9122c2fc323cc84330b679f55f671fc58d
Rustam Valiullin <rvcoding@gmail.com>
2021-03-22 15:02:25 +0300

    removed duplicated pane-basic example

--- src/examples/example.module.ts
+++ src/examples/example.module.ts
@@ -14,7 +14,7 @@ import { ExampleActionBarBasicComponent } from './action-bar-basic/example-actio
 import { ExampleActionBarOnEnterComponent } from './action-bar-on-enter/example-action-bar-on-enter.component';
 import { ExampleActionBarTemplateComponent } from './action-bar-template/example-action-bar-template.component';
 import { ExampleAggregateColumnBasicComponent } from './aggregate-column-basic/example-aggregate-column-basic.component';
-import { ExampleAltBasicComponent } from './pane-basic/example-alt-basic.component';
+import { ExampleAltBasicComponent } from './alt-basic/example-alt-basic.component';
 import { ExampleCellTooltipBasicComponent } from './cell-tooltip-basic/example-cell-tooltip-basic.component';
 import { ExampleColumnArrayBasicComponent } from './column-array-basic/example-column-array-basic.component';
 import { ExampleColumnAutocompleteBasicComponent } from './column-autocomplete-basic/example-column-autocomplete-basic.component';
