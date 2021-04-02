365ddf82ca6b24ff5007d2bb6d8d166847cc3d4b
Rustam Valiullin <rvcoding@gmail.com>
2021-04-02 18:26:24 +0300

    mouse api example

---
+++ src/examples/mouse-api-basic/example-mouse-api-basic.component.ts
@@ -0,0 +1,37 @@
+import { ViewChild, Component, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
+import { DataService, Human } from '../data.service';
+import { Observable } from 'rxjs';
+import { GridComponent } from 'ng2-qgrid';
+
+const EXAMPLE_TAGS = [
+	'mouse-api-basic',
+	'Mouse api example'
+];
+
+@Component({
+	selector: 'example-mouse-api-basic',
+	templateUrl: 'example-mouse-api-basic.component.html',
+	styleUrls: ['example-mouse-api-basic.component.scss'],
+	providers: [DataService, GridComponent],
+	changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ExampleMouseApiBasicComponent implements AfterViewInit{
+	@ViewChild(GridComponent) grid: GridComponent;
+
+	static id = EXAMPLE_TAGS[0];
+
+	title = EXAMPLE_TAGS[1];
+	rows: Observable<Human[]>;
+
+	constructor(dataService: DataService) {
+		this.rows = dataService.getPeople();
+	}
+	
+	ngAfterViewInit(): void {
+		const { model } = this.grid;
+		model.mouseChanged.on(({ state }) => {
+			const { status } = state;
+			console.log(status);
+		});
+	}
+}
