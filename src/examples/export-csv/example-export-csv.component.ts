701d2153dc38c4ca1d4dff1df20cdec84eccf188
Rustam Valiullin <rvcoding@gmail.com>
2021-05-14 16:10:15 +0300

    example-export-csv updated

--- src/examples/export-csv/example-export-csv.component.ts
+++ src/examples/export-csv/example-export-csv.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ViewChild, ChangeDetectionStrategy } from '@angular/core';
 import { Observable } from 'rxjs';
-import { GridComponent } from 'ng2-qgrid';
+import { Grid, GridModel } from 'ng2-qgrid';
 import { Atom, DataService } from '../data.service';
 import * as fileSaver from 'file-saver';
 
@@ -20,15 +20,18 @@ export class ExampleExportCsvComponent implements AfterViewInit {
 	static tags = EXAMPLE_TAGS;
 	title = EXAMPLE_TAGS[1];
 
-	@ViewChild(GridComponent) grid: GridComponent;
 	rows: Observable<Atom[]>;
+	gridModel: GridModel;
 
-	constructor(dataService: DataService) {
+	constructor(dataService: DataService,
+		private qgrid: Grid
+	) {
 		this.rows = dataService.getAtoms();
+		this.gridModel = qgrid.model();
 	}
 
 	ngAfterViewInit() {
-		this.grid.model.plugin({
+		this.gridModel.plugin({
 			imports: {
 				'fileSaver': fileSaver
 			}
