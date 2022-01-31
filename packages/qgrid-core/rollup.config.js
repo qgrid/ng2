506739b4da5ce7b5ea367a917f05032dec143f6e
dmitry <dmitry.sitnov@arcadia.spb.ru>
2022-01-31 23:27:39 +0300

    bundle core deps

--- packages/qgrid-core/rollup.config.js
+++ packages/qgrid-core/rollup.config.js
@@ -1,27 +1,27 @@
-// import commonjs from '@rollup/plugin-commonjs';
-// import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import dts from 'rollup-plugin-dts';
 //import { terser } from 'rollup-plugin-terser';
 
 export default [
-  {
-    input: './public-api.js',
-    output: {
-      file: './dist/public-api.js',
-      format: 'es',
-      name: '@qgrid/plugins',
-  //    plugins: [terser()]
-    }
-  },
-  {
-    input: "./public-api.d.ts",
-    output: [
-      {
-        file: "./dist/public-api.d.ts", 
-        format: "es",
-        name: '@qgrid/plugins'
-      }
-    ],
-    plugins: [dts()],
-  }
+	{
+		input: './public-api.js',
+		output: {
+			file: './dist/public-api.js',
+			format: 'es',
+		},
+		plugins: [
+			nodeResolve({ preferBuiltins: false, browser: true, }),
+			commonjs(),
+		]
+	},
+	{
+		input: './public-api.d.ts',
+		output: {
+			file: './dist/public-api.d.ts',
+		},
+		plugins: [
+			dts()
+		],
+	}
 ];
