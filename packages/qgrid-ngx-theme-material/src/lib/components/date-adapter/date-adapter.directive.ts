d2551019df211b82c289ba517f6fbdf04e60d665
Dalantren <ivanita@labcorp.com>
2022-05-05 15:45:12 +0300

    Fixed qgrid-ngx-theme-material

--- packages/qgrid-ngx-theme-material/src/lib/components/date-adapter/date-adapter.directive.ts
+++ packages/qgrid-ngx-theme-material/src/lib/components/date-adapter/date-adapter.directive.ts
@@ -4,23 +4,23 @@ import { DateAdapter } from '@angular/material/core';
 import { FormatDateAdapter } from './format-date-adapter';
 
 @Directive({
-	selector: '[q-grid-date-adapter]',
-	providers: [DatePipe, { provide: DateAdapter, useClass: FormatDateAdapter } ],
+  selector: '[q-grid-date-adapter]',
+  providers: [DatePipe, { provide: DateAdapter, useClass: FormatDateAdapter }],
 })
 export class DateAdapterDirective implements OnChanges {
-	@Input('q-grid-date-adapter') format: string;
+  @Input('q-grid-date-adapter') format: string;
 
-	constructor(
-		datePipe: DatePipe,
-		private adapter: DateAdapter<Date>) {
-		const formatAdapter = this.adapter as FormatDateAdapter;
-		formatAdapter.datePipe = datePipe;
-	}
+  constructor(
+    datePipe: DatePipe,
+    private adapter: DateAdapter<Date>) {
+    const formatAdapter = this.adapter as FormatDateAdapter;
+    formatAdapter.datePipe = datePipe;
+  }
 
-	ngOnChanges(changes: SimpleChanges) {
-		if (changes.format) {
-			const formatAdapter = this.adapter as FormatDateAdapter;
-			formatAdapter.useFormat = this.format;
-		}
-	}
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.format) {
+      const formatAdapter = this.adapter as FormatDateAdapter;
+      formatAdapter.useFormat = this.format;
+    }
+  }
 }
