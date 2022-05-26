4035bf8bc731266cb4fea253beed45a062c37079
Artem <dalantren@yandex.ru>
2022-05-27 00:51:24 +0300

    Updated return type of getErrors() for Validator

--- packages/qgrid-core/src/validation/validation.service.d.ts
+++ packages/qgrid-core/src/validation/validation.service.d.ts
@@ -1,6 +1,6 @@
 export declare interface Validator {
-	validate(value: any): boolean;
-	getErrors(): Array<string>;
+  validate(value: any): boolean;
+  getErrors(): { [key: string]: (Array<string> | string) };
 }
 
 export declare function hasRules(rules: any, key: string): boolean;
