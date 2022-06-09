24b429345e2cf01bbc44cf704b8a15c4e61555cd
Igor Yakovenko <31434462+FinalButterfly@users.noreply.github.com>
2022-06-09 16:36:34 +0500

    Comment corrections, first pass

--- packages/qgrid-core/src/infrastructure/composite.d.ts
+++ packages/qgrid-core/src/infrastructure/composite.d.ts
@@ -1,7 +1,7 @@
 import { Command } from '../command/command';
 
 export declare class Composite {
-  static func<T, A>(list: ((...args: any) => T)[], reducer?: (A: any, T: any) => A, memo?: A): (...args: any) => A;
+  static func<T, A>(list: ((...args: A) => T)[], reducer?: (a: A, t: T) => A, memo?: A): (...args: A) => T;
   static command(list: Command[]): Command;
   static list(list: any[]): any[];
   static object(list: any[], memo: any): any;
